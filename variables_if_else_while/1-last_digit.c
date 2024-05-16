31e50264c01b27d57326dee792537ecf0cc95202
internashionalist <taylor.thames@atlasschool.com>
2024-05-16 21:30:53 +0000

    1.2 - brackets around ifs

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,13 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	z = (n % 10)
+	z = (n % 10);
 	printf("Last digit of %d is %d ", n, z);
 		if (z > 5);
+		{
 			printf("and is greater than 5\n");
+		}
 		else if (z == 0);
-			printf("and is 0\n");
+		{
+			printf("and is 0\n")
+		}
 		else (z != 0 && z < 6);
+		{
 			printf("and is less than 6 and not 0\n", n);
+		}
 	return (0);
 }
