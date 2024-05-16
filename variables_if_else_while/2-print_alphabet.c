c32fdf09bb4ab99d89c4125d49db7ccb4434a88f
internashionalist <taylor.thames@atlasschool.com>
2024-05-16 22:10:31 +0000

    2.0

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point - prints alphabet in lower case
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	return (0)
+}
