8721d1e3c5662bb7eb1c6cde99c0391890e3cb22
internashionalist <taylor.thames@atlasschool.com>
2024-05-16 20:48:40 +0000

    d to n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("%n is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("%n is negative\n", n);
 	else
-		printf("%d is zero\n");
+		printf("%n is zero\n");
 	return (0);
 }
