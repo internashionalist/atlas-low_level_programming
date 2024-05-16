abc392a4bf0984b31d7a013ac68c9f5b67638c1c
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-16 16:56:43 +0000

    6.1 - corrected copypaste error

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
-	printf("Size of a char: %ld byte(s)\n", sizeof(int));
-	printf("Size of a char: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a char: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a char: %ld byte(s)\n", sizeof(float));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
