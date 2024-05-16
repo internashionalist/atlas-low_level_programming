5c9fb6cb468869f63743e4154a7ad961dc96b505
internashionalist <taylor.thames@atlasschool.com>
2024-05-16 04:50:14 +0000

    5.1 - corrected phrase

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
