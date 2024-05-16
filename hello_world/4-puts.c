32f0fcc7adfb93d0cb11d49329414ac4e11b19ce
internashionalist <taylor.thames@atlasschool.com>
2024-05-16 01:15:24 +0000

    4.1 - betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a multilingual
+ *                puzzle"
+ *
+ * return - 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
