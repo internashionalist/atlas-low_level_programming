64beabf411d44aa96f5a4462e4ea8bd836dab7aa
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-16 04:39:56 +0000

    5.0

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
