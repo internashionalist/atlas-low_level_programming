81d93b42b76e5fe79031585ab3a306c1215053d7
internashionalist <taylor.thames@atlasschool.com>
2024-05-16 19:58:10 +0000

    0.0 - not betty compliant

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+        	printf("%d is positive\n", n);
+
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	
+	else
+        	printf("%d is zero\n");
+
+	return (0);
+}
