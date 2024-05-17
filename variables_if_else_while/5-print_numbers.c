6489a872a65af7e7954598e21bafb223b536137c
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-17 00:10:45 +0000

    5.1 - compiling errors

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	int i
-	for (i = 0, i < 10, i++)
-	{
-	printf("%i", i);
-	}
-	printf("\n");
-	return (0);
+int i
+for (i = 0, i < 10, i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
 }
