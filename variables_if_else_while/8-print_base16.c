d9bb62e06b541e6f0c308f113db2e2817cb2710f
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-17 01:56:58 +0000

    8.0 - hexadecimal sucks

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar (i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar (j);
+	putchar('\n');
+	return (0);
