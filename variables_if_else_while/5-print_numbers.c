350bd5802c8712e9113e9136e7ccb80f11345f94
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-17 00:08:51 +0000

    5.0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point - prints all single digit base 10 numbers 0->
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i
+	for (i = 0, i < 10, i++)
+	{
+	printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
