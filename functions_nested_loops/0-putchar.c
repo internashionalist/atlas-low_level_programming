b627db39434bacfd0a36bde29328f5297faf8292
internashionalist <taylor.thames@atlasschool.com>
2024-05-17 04:41:56 +0000

    0.0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - prints "_putchar \n" without any libraries
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str = "_putchar";
+
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	return (0);
+}
