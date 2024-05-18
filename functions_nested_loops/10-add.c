aaba874121e3ae170e95e293a8d88902cb9ea58e
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-18 07:31:16 +0000

    10.0 - suspiciously easy

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two given integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of a + b
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
