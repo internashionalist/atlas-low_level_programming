2b53725a569f71a63507761b63e19bfdb0edde85
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-18 08:47:40 +0000

    11.1 - mixed up <>

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,15 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n < 98)
-			printf("%d, ", n++);
+		while (n > 98)
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 
 	else
 	{
-		while (n > 98)
-			printf("%d, ", n--);
+		while (n < 98)
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 }
