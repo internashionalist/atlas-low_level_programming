a69c5d0d9787572d8bd501fca9bb7e0ed6833726
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-18 07:22:07 +0000

    9.0 - on a roll

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,8 +2,28 @@
 
 /**
  * times_table - prints the 9 times table starting with 0
- * 		in given format
+ *		in given format
  */
 
 void times_table(void)
 {
+	int a, b, ab;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			ab = a * b;
+			if (ab <= 9)
+				_putchar(' ');
+			else
+				_putchar((ab / 10) + '0');
+
+			_putchar((ab % 10) + '0');
+		}
+	}
+}
