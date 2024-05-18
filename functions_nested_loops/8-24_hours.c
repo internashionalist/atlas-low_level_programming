a393f85fb245ddb6f89cb7d5458384c3dab04839
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-18 06:17:48 +0000

    8.0

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,3 +7,18 @@
 
 void jack_bauer(void)
 {
+	int HR;
+	int MIN;
+
+	for (HR = 0; HR <= 23; HR++)
+	{
+		for (MIN = 0; MIN <= 59; MIN++)
+		{
+			_putchar((HR / 10) + '0');
+			_putchar((HR % 10) + '0');
+			_putchar(':');
+			_putchar((MIN / 10) + '0');
+			_putchar((MIN % 10) + '0');
+		}
+	}
+}
