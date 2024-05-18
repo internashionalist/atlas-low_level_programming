382b3c9d0adc0fbfb1c72ff3c62e328efe74a346
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-18 04:19:54 +0000

    6.0 - ..i forgot what absolute numbers are

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an integer
+ * @i: integer to be used
+ * Return: absolute value of i
+ */
+
+int _abs(int i)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
