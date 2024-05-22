06587d0173675f4ce6f5e070d7c1135fc72bed42
internashionalist <taylor.thames@atlasschool.com>
2024-05-22 21:04:26 +0000

    7.1 - what the fuck

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,4 +9,25 @@
 
 void puts_half(char *str)
 {
+	int i = 0;
+	int k;
+
+	for (i = 0; str[i] != '\0'; i++)
+		i++;
+
+	if (i % 2 == 1)
+	{
+		k = (i - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = i / 2;
+	}
+	for (; k < i; k++)
+	{
+		_putchar(str[k]);
+	}
+
+	_putchar('\n');
 }
