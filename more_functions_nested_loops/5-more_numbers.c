ae57003cb055ede8315e435dfa0895050211a825
internashionalist <taylor.thames@atlaschool.com>
2024-05-22 06:36:08 +0000

    5.0 - why cant i run my shit and why is that + 0 necessary

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,4 +8,19 @@
 
 void more_numbers(void)
 {
+	int i;
+	int k;
+
+	for (k = 0 ; k <= 9 ; k++)
+	{
+		for (i = 0 ; i <= 14 ; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+		_putchar((i % 10) + '0');
+		}
+	_putchar('\n');
+	}
 }
