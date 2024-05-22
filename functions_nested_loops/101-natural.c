00acccb343c60fef0f1cc615c0d63d415cecf011
internashionalist <taylor.thames@atlasschool.com>
2024-05-22 04:17:36 +0000

    101.0 - doesnt seem right

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all multiples
+ *	of 3 and 5 below 1024 (excluded)
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
