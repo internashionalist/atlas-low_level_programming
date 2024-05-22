8d3d499b6723b675d04d27644cea76cba01412cf
internashionalist <taylor.thames@atlasschool.com>
2024-05-22 23:35:53 +0000

    8.0 - base

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an integer array
+ * @a: integer array
+ * @n: printed number of elements
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+}
