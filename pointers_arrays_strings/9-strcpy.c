57795214425320a4f9a6e590800cd392179e7db7
internashionalist <taylor.thames@atlaschool.com>
2024-05-22 23:38:24 +0000

    9.0 - base

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies one string to another string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+}
