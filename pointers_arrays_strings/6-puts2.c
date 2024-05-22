75f2b3e93ecf9eba75bff8e9b1578a8c3f6bf19a
internashionalist <taylor.thames@atlasschool.com>
2024-05-22 20:16:25 +0000

    6.1 - sort of like Task 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
 * puts2 - prints every other character in string
@@ -6,9 +7,17 @@
 *	followed by new line
 * @str: string to be printed
 *
-* Return: Always 0
+* Return: void
 */
 
 void puts2(char *str)
 {
+	int l;
+
+	for (l = 0 ; l < _strlen(str) ; l += 2)
+	{
+		_putchar(str[l]);
+	}
+
+	_putchar('\n');
 }
