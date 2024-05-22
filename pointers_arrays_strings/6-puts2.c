552b5200545479dc382dd87a7b089239e318dd40
internashionalist <taylor.thames@atlasschool.com>
2024-05-22 19:53:08 +0000

    6.0 - base

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character in string
+*	starting with first character
+*	followed by new line
+* @str: string to be printed
+*
+* Return: Always 0
+*/
+
+void puts2(char *str)
+{
+}
