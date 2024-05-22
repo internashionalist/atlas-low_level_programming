1ce17b79f93b8196435bf8414f3c524a2a0d2aa1
internashionalist <taylor.thames@atlaschool.com>
2024-05-22 09:40:53 +0000

    10.0 - i know... the spaces

---
+++ more_functions_nested_loops/10-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints 1-100
+ *	fizz for mult of 3
+ *	buzz for mult of 5
+ *	fizzbuzz for both
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 15  == 0)
+		{
+			printf("FizzBuzz");
+		}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (i % 5 == 0 && i != 100)
+			{
+				printf("Buzz");
+			}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
