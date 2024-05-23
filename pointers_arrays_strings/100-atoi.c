b1537b891b2731ad19b10c42ad7736e18e419f9c
internashionalist <taylor.thames@atlaschool.com>
2024-05-23 01:44:59 +0000

    6.1 - esrever

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,9 +3,9 @@
 /**
  * _atoi - converts string to an integer.
  * @s: string
+ *
  * Return: integer
  */
 
 int _atoi(char *s)
-{
-}
+
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -4,9 +4,21 @@
 * rev_string - prints string reversed
 * @s: string
 *
-* Return: Always 0
+* Return: void
 */
 
 void rev_string(char *s)
 {
+	int l = 0;
+	int i = 0;
+	char tmp;
+
+	while (s[i++])
+		l++;
+	for (i = l - 1 ; i >= l / 2 ; i--)
+	{
+		tmp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tmp;
+	}
 }
