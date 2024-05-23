ed8bb7519c3058534f19044cc1eeb4c16a2eb3a2
internashionalist <taylor.thames@atlasschool.com>
2024-05-23 06:57:41 +0000

    100.0

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor of 612852475143
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	long long i = 612852475143;
+
+	while (i % 2 == 0)
+	{
+		i /= 2;
+	}
+
+	for (long long k = 3; k * k <= num; k += 2)
+	{
+		while (i % k == 0)
+	{
+		i /= k;
+	}
+}
+	printf("%lld\n", i);
+
+	return (0);
+}
