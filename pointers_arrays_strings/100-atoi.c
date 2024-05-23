e188a457c51dc01513fa50176a8267e82a14f6a8
internashionalist <taylor.thames@atlaschool.com>
2024-05-23 04:02:03 +0000

    BETTTYYYYY

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,16 +12,14 @@ int _atoi(char *s)
 	int sign = 1; /* determines if final integer is pos or neg */
 	unsigned int n = 0; /* holds converted number */
 
-	do /* loop iterates over each char of string s points to until end of str */
-	{
+	do { /* loop iterates over each char of string s points to until end of str */
 		if (*s == '-')
 			sign *= -1; /* flips sign if neg */
 		else if (*s >= 48 && *s <= 57) /* checks if digit */
 			n = (n * 10) + (*s - 48); /* adds left-shifted integer to n */
 		else if (n > 0) /* if not digit or 0, breaks loop */
 			break;
-	}
-       	while (*s++); /* moves to next char */
+	} while (*s++); /* moves to next char */
 
 	return (n * sign); /* returns n*sign, which is pos or neg depending on str */
 }
