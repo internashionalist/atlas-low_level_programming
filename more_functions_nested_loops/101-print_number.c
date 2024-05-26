b836f8ab3ea84078a8e86e3d110eb384a6beb051
internashionalist <taylor.thames@atlasschool.com>
2024-05-26 12:21:19 +0000

    101.6 - i dont even care how

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,21 +9,14 @@
 
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	while (n > 0)
-	{
-		_putchar((n % 10) + '0');
-		n /= 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
 }
