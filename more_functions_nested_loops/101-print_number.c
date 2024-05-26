2c9adff8a49876b08b43ed16de603825057d831d
internashionalist <taylor.thames@atlasschool.com>
2024-05-26 20:33:37 +0000

    101.12 - delete and remake

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -4,25 +4,26 @@
  * print_number - prints an integer
  * @n: integer to be printed
  */
+
 void print_number(int n)
 {
-	unsigned int m;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		m = -n;
+		n1 = -n;
 		_putchar('-');
-	} 
+	}
 
 	else
 	{
-		m = n;
+		n1 = n;
 	}
 
-	if (m / 10)
+	if (n1 / 10)
 	{
-		print_number(m / 10);
+		print_number(n1 / 10);
 	}
 
-	_putchar((m % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
