3c481238f46721f0f7fd0ece204a6d67fbd3904f
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-26 12:23:31 +0000

    101.7 - i must beat this checker

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -2,21 +2,25 @@
 
 /**
  * print_number - prints an integer.
- * @n: integer to be printed
+ * @n: integer given to be printed
  *
  * Return: void
  */
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (n < 0 && n > -2147483648)
 	{
+		n = -(n);
 		_putchar('-');
-		n = -n;
 	}
 
 	if (n / 10)
 		print_number(n / 10);
 
-	_putchar((n % 10) + '0');
+	if (n == -2147483648)
+		_putchar((n % 10) + 64);
+
+	else
+		_putchar((n % 10) + '0');
 }
