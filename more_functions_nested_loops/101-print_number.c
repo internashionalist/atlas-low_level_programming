3e4536d5985ec5b7c2b8ed0931246d4e82e400ce
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-26 19:25:32 +0000

    101.8 - one more try

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,15 +9,23 @@
 
 void print_number(int n)
 {
+	unsigned int m;
+
 	if (n < 0)
 	{
-		n = -(n);
+		m = -n;
 		_putchar('-');
-	}
-
-	if (n / 10)
-		print_number(n / 10);
+	} 
 
 	else
-		_putchar((n % 10) + '0');
+	{
+		m = n;
+	}
+
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+
+	_putchar((m % 10) + '0');
 }
