dcaf1f9207bfa424d53f1a2de0257ff40b1c3c3e
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-26 12:01:17 +0000

    NO LONGS

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,19 @@
 
 void print_number(int n)
 {
+	int digit;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		n = -n;
 	}
 
-	if (n / 10 > 0)
+	while (n > 0)
 	{
-		print_number(n / 10);
+		digit = n % 10;
+		n /= 10;
+	
+	_putchar(digit + '0');
 	}
-
-	_putchar(n % 10 + '0');
 }
