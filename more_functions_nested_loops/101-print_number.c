4b14a60331487e89d7e4e12a6c0bb16e8373c987
internashionalist <taylor.thames@atlasschool.com>
2024-05-27 18:07:45 +0000

    101.15

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,24 +7,18 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	if (n < 0)
+	if (n < 0 && n > -2147483648)
 	{
-		n1 = -n;
 		_putchar('-');
+		n = -(n);
 	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	if (n == -2147483648)
+		_putchar((n % 10) + 64);
 
 	else
-
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+		_putchar((n % 10) + '0');
 }
