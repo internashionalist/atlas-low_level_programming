b833bfc96c708142708138779730a06e4ffcb299
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-29 08:55:57 +0000

    4.0 - base

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses content of an array
+* @a: string to be reversed
+* @n: num of elements of the array
+*
+* Return: Always 0
+*/
+
+void reverse_array(int *a, int n)
+{
+
