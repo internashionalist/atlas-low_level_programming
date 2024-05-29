fce432b314ada517ff164460ae4b26a0348b7e62
internashionalist <taylor.thames@atlasschool.com>
2024-05-29 08:53:05 +0000

    3.0 - that one took a while

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char *str;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
