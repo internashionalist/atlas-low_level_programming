3f9eae185e31f3f2a1363e84230c7dc86b1ff458
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-29 09:31:17 +0000

    5.1 - it cant be that easy

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,8 +4,22 @@
 * string_toupper - converts lowercase chars to uppercase
 * @str: string to be converted
 *
-* Return: Always 0
+* Return: converted string
 */
 
 char *string_toupper(char *str)
 {
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
