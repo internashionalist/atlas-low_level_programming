e19f46d031b6cbc38b36852746e059df3f0b77b0
internashionalist <taylor.thames@atlasschool.com>
2024-05-29 05:42:50 +0000

    0.0 - README, main.h, base

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates (a new word I know now) two strings
+ * @dest: first string.
+ * @src: second string.
+ *
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+
