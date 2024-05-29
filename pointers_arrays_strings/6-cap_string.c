c2bc3c24ef15c08ad16c232773e635cce9c075b2
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-29 17:08:08 +0000

    6.2 - more progress

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,3 +17,14 @@ char *cap_string(char *str)
 	{
 		for (j = 0; separator[j]; j++)
 		{
+			if (str[j] == separator[i - 1] && str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}
