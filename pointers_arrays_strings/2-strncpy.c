f6bd9111eff750485828e8cb18afcfd5c2570dea
internashionalist <taylor.thames@atlasschool.com>
2024-05-29 06:47:51 +0000

    2.0 - base

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string
+* @dest: destination string
+* @src: source string
+* @n: byte number able to be copied in src
+*
+* Return: Always 0
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
