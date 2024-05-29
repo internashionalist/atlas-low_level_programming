38ec287a36ab5da3fa8865ac691946ab14b2bbd7
internashionalist <taylor.thames@atlasschool.com>
2024-05-29 09:35:42 +0000

    6.0 - base

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes Each Word In A String
+ * @str: Pointer To String To Be Capitalized
+ *
+ * Return: String With Capitalized Words
+ */
+
+char *cap_string(char *str)
+{
