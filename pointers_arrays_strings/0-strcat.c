e4c62598afd864a682f12b0a15f5e8ecdb347055
“internashionalist” <taylor.thames@atlaschool.com>
2024-05-29 06:06:23 +0000

    0.1 - loop consistency, I guess?

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates (a new word I know now) two strings
- * @dest: first string.
- * @src: second string.
+ * _strcat - concatenates (a new word I know now) two strings + null byte
+ * @dest: destination string
+ * @src: source string
  *
  * Return: pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
-
 {
+	int i = 0;
+	int j = 0;
 
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	return (dest);
+}
