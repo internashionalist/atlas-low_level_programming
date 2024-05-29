b9b8f4d4908858ee69d460b9f968998cf600666c
internashionalist <taylor.thames@atlasschool.com>
2024-05-29 15:13:00 +0000

    7.1 - 1337 as hell

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,3 +9,23 @@
 
 char *leet(char *str)
 {
+	int i = 0, j;
+	char standard[] = "AaEeOoTtLl";
+	char leet[] = "4433007711";
+
+	while (str[i])
+	{
+		for (j = 0; standard[j]; j++)
+		{
+			if (str[i] == standard[j])
+			{
+				str[i] = leet[j];
+			}
+		}
+
+		i++;
+	}
+
+	return (str);
+}
+
