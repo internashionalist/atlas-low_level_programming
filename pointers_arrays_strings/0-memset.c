db73f6b3749d8d8fd72699784ff1a69899df74f5
internashionalist <taylor.thames@atlasschool.com>
2024-05-31 18:58:30 +0000

    0.0 - base

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area pointed to
+ * @b: constant byte
+ * @n: number of bytes
+ *
+ * Return: pointer to memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+{
+}
