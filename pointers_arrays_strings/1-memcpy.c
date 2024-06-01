cf38cbf7a3c82bcdfac71c9f614e745467cf1839
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-01 05:08:19 +0000

    1.2 - copy n bytes per counter to dest from src

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,8 +2,8 @@
 
 /**
  * _memcpy - copies n bytes of memory area
- * @dest: copy destination
- * @src: source copied
+ * @dest: pointer to copy destination
+ * @src: pointer to source copied
  * @n: number of bytes copied
  *
  * Return: pointer to dest memory area
@@ -14,6 +14,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
 
 	return (dest);
 }
