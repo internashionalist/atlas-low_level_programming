16c9cec69b8b0cb14ace49c68679dbd7911365df
internashionalist <taylor.thames@atlasschool.com>
2024-06-01 06:58:55 +0000

    3.4 - stop when found, output doesnt match?

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: pointer to checked string
  * @accept: pointer to string with accepted characters
  *
- * Return: number of bytes from accept found in s
+ * Return: length of first byte from accept found in s
  * 
  */
 
@@ -19,6 +19,9 @@ unsigned int _strspn(char *s, char *accept)
 			for (k = 0; *(accept + k) != '\0'; k++) /* loop through accept */
 			{
 				if (*(s + len) == *(accept + k)) /* search for first accept byte in s */
+				{
+					break; /* when found, stop searching */
+				}
 			}
 		}
 	
