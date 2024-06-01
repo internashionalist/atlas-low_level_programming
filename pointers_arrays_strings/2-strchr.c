f621614bfdf001275a23f91039571a4cda061b11
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-01 05:35:54 +0000

    2.0 - base

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strchr - locates character in given string
+ * @s: string being checked
+ * @c: character checked for
+ *
+ * Return: pointer to first occurrence of c  or NULL if no c
+*/
+
+char *_strchr(char *s, char c)
+{
