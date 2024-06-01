b944d0a1f406fd8bf9a796b124999491620fbec6
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-01 06:15:13 +0000

    3.0 - base

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strspn - finds the length of a prefix substring
+ * @s: pointer to checked string
+ * @accept: pointer to string with accepted characters
+ *
+ * Return: number of bytes in s segment (only bytes from accept)
+ * 
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
