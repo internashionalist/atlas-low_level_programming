c6660afe058cdc1d836bcc0c046014e10d928735
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-01 06:22:24 +0000

    4.0 - base

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ **_strpbrk - searches a string for any of a given set of bytes
+ * @s: pointer to string checked for bytes
+ * @accept: pointer to string containing searched-for bytes
+ * 
+ * Return: pointer to byte in s matching accept, or NULL if none found
+ * 
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+}
