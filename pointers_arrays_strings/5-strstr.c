0e035730438f0b294c37cae9f9b99afa7fa360ba
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-01 21:16:41 +0000

    5.0 - base

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * * _strstr - locates a substring within a string
+ * @haystack: string searched
+ * @needle: substring searched for
+ *
+ * Return: pointer to beginning of substring or NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
