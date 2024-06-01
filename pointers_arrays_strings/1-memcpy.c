c7b8b14519c370dd704769a3a5c69ab463e59a24
internashionalist <taylor.thames@atlasschool.com>
2024-06-01 05:00:04 +0000

    1.0 - base

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes of memory area
+ * @dest: copy destination
+ * @src: source copied
+ * @n: number of bytes copied
+ *
+ * Return: pointer to dest memory area
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
