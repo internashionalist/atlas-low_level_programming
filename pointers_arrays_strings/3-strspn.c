40e284bf083f5d532a780d608f53396bf2dcf72f
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-01 20:42:37 +0000

    3.7 - added string counter, figured out break

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -6,21 +6,23 @@
  * @accept: pointer to string with accepted characters
  *
  * Return: number of bytes from accept string in s
- * 
+ *
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count; /* byte counter */
+	unsigned int count; /* length of byte counter */
+	unsigned int i; /* byte counter */
 	unsigned int k; /* accept counter */
 
-		for (count = 0; *(s + count) != '\0'; count++) /* loop through s */
+		for (i = 0; *(s + i) != '\0'; i++) /* loop through s */
 		{
 			for (k = 0; *(accept + k) != '\0'; k++) /* loop through accept */
 			{
-				if (*(s + count) == *(accept + k)) /* search for first accept byte in s */
+				if (*(s + count) == *(accept + k)) /* search for accept byte in s */
 				{
-					break; /* when found, stop searching */
+					count++; /* count increment */
+					break; /* stop counting */
 				}
 			}
 		}
