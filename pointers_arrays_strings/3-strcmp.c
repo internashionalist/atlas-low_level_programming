59bdec673f327b2dda6da1f8846b9864074bc9f6
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-02 01:48:18 +0000

    betty checks

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0; /* initialize and set counter to 0 */
 
-	while (*(s1 + i) != '\0' && *(s2 +i) != '\0') /* compare strings */
+	while (*(s1 + i) != '\0' && *(s2 + i) != '\0') /* compare strings */
 	{
-		if (*(s1 + i) != *(s2 +i)) /* if strings have differences */
+		if (*(s1 + i) != *(s2 + i)) /* if strings have differences */
 		{
 			return (*(s1 + i) - *(s2 + i)); /* return comparison */
 		}
