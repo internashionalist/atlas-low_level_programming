70b4fb8a9e0c50df3949dd8520d3a502568ad46f
internashionalist <taylor.thames@atlasschool.com>
2024-06-02 08:08:55 +0000

    7.1 - something wrong

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,10 +9,18 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i = 0; /* first counter */
-	unsigned int k = 0; /* second counter */
+	unsigned int i = 0; /* rows iterator */
+	unsigned int k = 0; /* columns iterator */
+
 
 	for (; i < 8; i++) /* is shorter better?? */
 	{
 		for (; k < 8; k++) /* hope this format works */
 		{
+			_putchar(a[i][k]); /* print arrays */
+			_putchar(' '); /* space after arrays */
+		}
+
+	_putchar('\n'); /* new line */
+	}
+}
