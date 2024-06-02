46836aaaa2828631bae02a2fdf52ed6dd1d13102
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-02 08:51:35 +0000

    6.8 - progress??

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,19 +15,19 @@ char *cap_string(char *str)
 	{
 		if /* if character BEFORE i is a separator */
 		(i == 0 ||
-		 str[i - 1] == ' ' ||
-		 str[i - 1] == '\t' ||
-		 str[i - 1] == '\n' ||
-		 str[i - 1] == ',' ||
-		 str[i - 1] == ';' ||
-		 str[i - 1] == '.' ||
-		 str[i - 1] == '!' ||
-		 str[i - 1] == '?' ||
-		 str[i - 1] == '"' ||
-		 str[i - 1] == '(' ||
-		 str[i - 1] == ')' ||
-		 str[i - 1] == '{'
-		 && (str[i] >= 'a') && (str[i] <= 'z')) /* and i is lowercase */
+		 (str[i - 1] == ' ') ||
+		 (str[i - 1] == '\t') ||
+		 (str[i - 1] == '\n') ||
+		 (str[i - 1] == ',') ||
+		 (str[i - 1] == ';') ||
+		 (str[i - 1] == '.') ||
+		 (str[i - 1] == '!') ||
+		 (str[i - 1] == '?') ||
+		 (str[i - 1] == '"') ||
+		 (str[i - 1] == '(') ||
+		 (str[i - 1] == ')') ||
+		 (str[i - 1] == '{')
+		 (&& str[i] >= 'a' && str[i] <= 'z')) /* and i is lowercase */
 		{
 			str[i] -= 32; /* capitalize i char */
 		}
