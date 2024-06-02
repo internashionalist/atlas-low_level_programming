aa73aef27dfda78a739d16a15b02fce0c6b00dc2
internashionalist <taylor.thames@atlasschool.com>
2024-06-02 08:46:45 +0000

    6.7 - I think it works but idk where to put parantheses to make it compile

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,24 +11,27 @@ char *cap_string(char *str)
 {
 	int i; /* initialize iterator */
 
-	for (int i = 0; str[i]; i++) /* iterate through string */
+	for (i = 0; str[i]; i++) /* iterate through string */
 	{
 		if /* if character BEFORE i is a separator */
 		(i == 0 ||
-		 (str[i-1] == ' ' ||
-		  str[i-1] == '\t' ||
-		  str[i-1] == '\n' ||
-		  str[i-1] == ',' ||
-		  str[i-1] == ';' ||
-		  str[i-1] == '.' ||
-		  str[i-1] == '!' ||
-		  str[i-1] == '?' ||
-		  str[i-1] == '"' ||
-		  str[i-1] == '(' ||
-		  str[i-1] == ')' ||
-		  str[i-1] == '{')
-		 && (str[i] >= 'a' && str[i] <= 'z')) /* and i is lowercase */
+		 str[i - 1] == ' ' ||
+		 str[i - 1] == '\t' ||
+		 str[i - 1] == '\n' ||
+		 str[i - 1] == ',' ||
+		 str[i - 1] == ';' ||
+		 str[i - 1] == '.' ||
+		 str[i - 1] == '!' ||
+		 str[i - 1] == '?' ||
+		 str[i - 1] == '"' ||
+		 str[i - 1] == '(' ||
+		 str[i - 1] == ')' ||
+		 str[i - 1] == '{'
+		 && (str[i] >= 'a') && (str[i] <= 'z')) /* and i is lowercase */
+		{
+			str[i] -= 32; /* capitalize i char */
 		}
 	}
 
 	return (str); /* return string with capitalized chars */
+}
