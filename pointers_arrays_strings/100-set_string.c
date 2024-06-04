b5f50fb52a0ac68b897eba7554a89c7662f19029
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-04 01:00:10 +0000

    100.0

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a given pointer to a char
+ * @s: double pointer to change
+ * @to: char to change s to
+ *
+ * Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to
+}
