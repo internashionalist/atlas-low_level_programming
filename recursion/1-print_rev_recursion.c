5a776a1e0fc0b8ab158c1a93401d8462c74c1a67
internashionalist <taylor.thames@atlasschool.com>
2024-06-05 06:24:21 +0000

    1.1 - I think I get it!

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,3 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
+	if (*s != '\0') /* BASE - stops at null byte */
+	{
+		_print_rev_recursion(s + 1); /* RECURSIVE CALL - move to next char through string */
+		_putchar(*s); /* print reversed string */
+	}
+}
