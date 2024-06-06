24c5d7bfc5e7f01acb94a5743b16b6a064e0bd77
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-06 08:43:46 +0000

    5.2 - cleaner

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,14 +9,14 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
 
-	if (n > 0)
+	else if (n == 0)
 	{
-		return (-1);
+		return (0);
 	}
 
 	return (n * _sqrt_recursion(n - 1));
