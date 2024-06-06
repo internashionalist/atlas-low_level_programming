9f0612a7b2ef67302c3ec780730dc536f730d002
internashionalist <taylor.thames@atlasschool.com>
2024-06-06 09:12:40 +0000

    5.7 - too much coffee. this is dumb.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,17 +3,25 @@
 /**
  * _sqfinder - differentiates natural roots from approximates
  * @maybe: maybe natural root, maybe not
- * perfect: perfect square
+ * @perfect: perfect square
  *
  * Return: maybe if natural square root, -1 if not
  */
 
 int _sqfinder(int maybe, int perfect)
 {
-	if ((maybe * maybe) == perfect)
+	if ((maybe * maybe) == perfect) /* if approximate is NATURAL ROOT */
 	{
-		return (maybe);
+		return (maybe); /* return that */
 	}
+
+	if ((maybe * maybe) > perfect) /* if NOT */
+	{
+		return (-1); /* return error */
+	}
+
+	return (_sqfinder(perfect, maybe + 1)); /* recurse through, increment */
+}
 
 /**
  * _sqrt_recursion - finds an integers square root
@@ -29,12 +37,12 @@ int _sqrt_recursion(int n)
 		return (-1); /* no natural root */
 	}
 
-	else if (n == 0) /* if 0 */
+	if (n == 0) /* if 0 */
 	{
 		return (0); /* return 0 */
 	}
 
-	else if (n == 1) /* if 1 */
+	if (n == 1) /* if 1 */
 	{
 		return (1); /* wait why do I need this */
 	}
