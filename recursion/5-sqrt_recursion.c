9ba235bfab66135799d2a0033d7ea29f8fd4b760
internashionalist <taylor.thames@atlasschool.com>
2024-06-06 08:35:31 +0000

    5.1 - doesnt find natural roots

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - finds an integers square root
+ * _sqrt_recursion - finds an integers natural square root
  * @n: given int
  *
- * Return: sqare root of integer
+ * Return: natural sqare root of integer
 */
 
 int _sqrt_recursion(int n)
 {
+	if (n == 0)
+	{
+		return (-1);
+	}
+
+	if (n > 0)
+	{
+		return (-1);
+	}
+
+	return (n * _sqrt_recursion(n - 1));
+}
