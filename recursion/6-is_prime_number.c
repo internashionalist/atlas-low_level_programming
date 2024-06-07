374a010884f6fd20b6b37b5a07df12412c9a4401
internashionalist <taylor.thames@atlasschool.com>
2024-06-07 09:40:30 +0000

    6.1 - basic ifs

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,3 +9,20 @@
 
 int is_prime_number(int n)
 {
+	if (n <= 1) /* exclude negatives, 1 */
+	{
+		return (0);
+	}
+
+	if (n % 2 == 0) /* exclude evens */
+	{
+		return (0);
+	}
+
+	if (n == 2) /* include 2 though */
+        {
+                return (1);
+        }
+
+	return (is_divisible(n, 3)); /* CALL - start at 3 */
+}
