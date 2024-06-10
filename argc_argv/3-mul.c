3f3e1200ade11499e89719f0f7f8edca4ff6101c
internashionalist <taylor.thames@atlasschool.com>
2024-06-10 16:08:52 +0000

    4.0 - base

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -15,14 +15,14 @@ int main(int argc, char *argv[])
 	int a;
 	int b;
 
-	if (argc != 3)
+	if (argc != 3) /* if != 3 args */
 	{
-		printf("Error\n");
+		printf("Error\n"); /* print error and \n */
 
 		return (1);
 	}
 
-	a = atoi(argv[1]);
+	a = atoi(argv[1]); /* glad I learned atoi */
 	b = atoi(argv[2]);
 
 	printf("%d\n", (a * b));
---
+++ argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - adds two postive numbers
+ * @argc: arguments
+ * @argv: strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
