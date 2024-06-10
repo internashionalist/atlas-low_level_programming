d79b5bdafe768b79e041b570c3ca174513c09662
internashionalist <taylor.thames@atlasschool.com>
2024-06-10 16:03:11 +0000

    3.2 - hopefully this Error output is correct

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -17,6 +18,14 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", (a * b));
+
+	return (0);
 }
