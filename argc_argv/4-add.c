2d5b790362fcf03152461985a0612c5a9bef1675
internashionalist <taylor.thames@atlasschool.com>
2024-06-10 16:12:37 +0000

    4.1 - first error

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,3 +11,13 @@
 
 int main(int argc, char *argv[])
 {
+	int a;
+	int b;
+	int sum = 0;
+
+	if (argc < 2) /* if program doesnt have enough args to run */
+	{
+		printf("0\n"); /* print 0 and \n */
+
+		return (0);
+	}
