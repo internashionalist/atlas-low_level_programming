eb3df06dd1c41da10045eb9c843c9acdb3bcc071
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-13 04:46:58 +0000

    0.0 - base

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * create_array - creates char array, initializes with specific char
+ * @size: size of array
+ * @c: specific char that initializes
+ *
+ * Return: pointer to the array on success, NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
