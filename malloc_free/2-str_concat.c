9689ebb0b20ef520ccaebc22f33021e5289f61fa
internashionalist <taylor.thames@atlasschool.com>
2024-06-13 15:03:30 +0000

    2.2 - vscode

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,13 +11,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0; /* iterator */
+	int j = 0; /* second iterator */
 	int s1size = 0; /* string 1 size counter */
 	int s2size = 0; /*string 2 size counter */
-	char *str = NULL;
+	char *doublecat = NULL; /* ITS TWO CATENATES */
 
 	if (s1 == NULL) /* if not present */
 	{
-		s1size = 0; /* count as zero */
+		s1size = 0; /* count as empty string */
 	}
 	else
 	{
@@ -34,9 +35,30 @@ char *str_concat(char *s1, char *s2)
 
 	else
 	{
-		for (i = 0; s2[i] != '\0'; i++)
+		for (i = 0; s2[i] != '\0'; i++) /* iterate, count */
 		{
 			s2size++;
 		}
 	}
+
+	doublecat = (char *)malloc(s1size + s2size + 1); /* allocate strs + null */
+
+	if (doublecat == NULL) /* if failed */
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < s1size; i++) /* copy first string */
+	{
+		doublecat[i] = s1[i];
+	}
+
+	for (j = 0; j < s2size; j++) /* go through with DIFFERENT ITERATOR */
+	{
+		doublecat[i + j] = s2[j];
+	}
+
+	doublecat[i + j] = '\0'; /* wait then do I need to allocate for it */
+
+	return (doublecat); /* please oh doublecat just compile */
 }
