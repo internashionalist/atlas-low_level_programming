8bbe4bd7f3136b403efbba1e8cb077dba9ec265c
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-13 06:16:15 +0000

    2.1 - first iterators and counters

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,3 +10,33 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	int i = 0; /* iterator */
+	int s1size = 0; /* string 1 size counter */
+	int s2size = 0; /*string 2 size counter */
+	char *str = NULL;
+
+	if (s1 == NULL) /* if not present */
+	{
+		s1size = 0; /* count as zero */
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++) /* iterate, count */
+		{
+			s1size++;
+		}
+	}
+
+	if (s2 == NULL) /* same with second string */
+	{
+		s2size = 0;
+	}
+
+	else
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			s2size++;
+		}
+	}
+}
