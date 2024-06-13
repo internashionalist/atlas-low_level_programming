b85eced6d1a98bc99f5289a22a04c9a9eaa4af20
internashionalist <taylor.thames@atlasschool.com>
2024-06-13 17:25:48 +0000

    4.0 - base

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * free_grid - frees 2D grid from alloc_free
+ * @grid: grid to be freed
+ * @height: pointers in grid
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
