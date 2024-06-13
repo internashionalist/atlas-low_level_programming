020c9106f91687d81545049a2d8e249526097404
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-13 04:50:22 +0000

    0.1 - first null case

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * create_array - creates char array, initializes with specific char
+ * create_array - creates array using chars, initializes with specific char
  * @size: size of array
- * @c: specific char that initializes
+ * @c: specific char that initializes, fills array
  *
  * Return: pointer to the array on success, NULL if it fails
  */
 
 char *create_array(unsigned int size, char c)
 {
+	if (size == 0)
+	{
+		return (NULL);
+	}
