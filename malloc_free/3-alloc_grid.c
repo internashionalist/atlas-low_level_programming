f69d3f608e9736919a56a801c71bb9bf82e1df06
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-13 15:18:56 +0000

    3.0 - base

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * alloc_grid - makes a grid
+ * @width: grid collumns
+ * @height: grid rows
+ * Return: pointer to grid, or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
