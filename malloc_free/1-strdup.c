2e94cc0d18228c3f4d38aa39f2bd411b61fbe2e8
internashionalist <taylor.thames@atlasschool.com>
2024-06-13 11:37:52 +0000

    1.3 - putting it all together

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,17 +12,25 @@
 
 char *_strdup(char *str)
 {
-	unsigned int i = 0;
-	unsigned int length = 0;
+	unsigned int i = 0; /* iterator */
+	unsigned int length = 0; /* length counter */
+	char *strcopy /* copied string */
 
-	if (str == NULL)
+	if (str == NULL) /* if string absent */
 	{
 		return (NULL);
 	}
 
-	while (str[length] != '\0')
+	while (str[length] != '\0') /* count length */
 	{
 		length++;
 	}
-}
 
+	strcopy = malloc(sizeof(char) * (length + 1)); /* allocate size, include null */
+
+	if (strcopy == NULL) /* if failed */
+	{
+		return (NULL);
+	}
+
+}
