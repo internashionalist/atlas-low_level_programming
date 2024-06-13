3e4b5f6123061db217760c323566367edd90dec5
internashionalist <taylor.thames@atlasschool.com>
2024-06-13 05:17:47 +0000

    2.0 - base

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings, new location
+ * @s1: concatenatee
+ * @s2: concatenator
+ *
+ * Return: pointer to new space with both strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
