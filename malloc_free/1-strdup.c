40ca6f53a95459c759b97b3f6f7526294802835d
internashionalist <taylor.thames@atlasschool.com>
2024-06-13 05:14:54 +0000

    1.0 - base

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strdup - returns pointer to newly-allocated space
+ * @str: str to be copied, used as parameter
+ *
+ *
+ * Return: pointer to new paramater
+ *
+ */
+
+char *_strdup(char *str)
+{
