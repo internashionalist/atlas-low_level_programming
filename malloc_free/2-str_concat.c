f69cd032f0d328a2be9d41f1b6beb1383e8e02e5
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-13 15:14:04 +0000

    2.5 betty hates good formatting

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,46 +17,30 @@ char *str_concat(char *s1, char *s2)
 	char *doublecat = NULL; /* ITS TWO CATENATES */
 
 	if (s1 == NULL) /* if not present */
-	{
 		s1size = 0; /* count as empty string */
-	}
+
 	else
-	{
 		for (i = 0; s1[i] != '\0'; i++) /* iterate, count */
-		{
 			s1size++;
-		}
-	}
 
 	if (s2 == NULL) /* same with second string */
-	{
 		s2size = 0;
-	}
 
 	else
-	{
 		for (i = 0; s2[i] != '\0'; i++) /* iterate, count */
-		{
 			s2size++;
-		}
-	}
+
 
 	doublecat = (char *)malloc(s1size + s2size + 1); /* allocate strs + null */
 
 	if (doublecat == NULL) /* if failed */
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < s1size; i++) /* copy first string */
-	{
 		doublecat[i] = s1[i];
-	}
 
 	for (j = 0; j < s2size; j++) /* go through with DIFFERENT ITERATOR */
-	{
 		doublecat[i + j] = s2[j];
-	}
 
 	doublecat[i + j] = '\0'; /* wait then do I need to allocate for it */
 
