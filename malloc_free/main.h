266e5d151b78af14f70f144c87a0c9b82df0cc00
internashionalist <taylor.thames@atlasschool.com>
2024-06-14 00:31:03 +0000

    3.2 - doesnt seem to compile

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -6,7 +6,7 @@
 char *create_array(unsigned int, char);
 char *_strdup(char*);
 char *str_concat(char*, char*);
-int **aloc_grid(int, int);
+int **alloc_grid(int, int);
 void free_grid(int**, int);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
