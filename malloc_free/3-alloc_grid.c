aaa9b679e28071bb2ff3261e41e75dcb618f6cfe
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-14 00:31:03 +0000

    3.2 - doesnt seem to compile

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,21 +1,49 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * alloc_grid - makes a grid
- * @width: grid collumns
+ * @width: grid columns
  * @height: grid rows
+ *
  * Return: pointer to grid, or NULL
  */
 
 int **alloc_grid(int width, int height)
 {
-	int **grid = NULL;
-	int i;
+	int **grid = NULL; /* pointer to grid */
+	int i; /* iterators */
 	int j;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0) /* if null or negative */
 	{
 		return (NULL);
 	}
 
-	grid = malloc(sizeof(int *) * height);	
+	grid = malloc(sizeof(int *) * height); /* allocate for rows */
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width); /* allocate for columns */
+
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+
+		free(grid);
+
+		return (NULL);
+		}
+
+		for (j = 0; j < width; j++) /* init to 0 */
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
