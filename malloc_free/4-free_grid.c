d2c63e161f204c386b64e5d03404df9322d94be0
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-14 05:00:45 +0000

    4.1 - easy one

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -10,3 +10,12 @@
 
 void free_grid(int **grid, int height)
 {
+	int i = 0; /* init counter to 0 */
+
+	for (; i < height; i++) /* cycle through */
+	{
+		free(grid[i]); /* free 2D grid */
+	}
+
+	free(grid);
+}
