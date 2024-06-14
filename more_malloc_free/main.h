a81da3ee929ae9ba2edd5be83882b7c069208239
internashionalist <taylor.thames@atlasschool.com>
2024-06-14 05:14:02 +0000

    README main.h

---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef main_h
+#define main_h
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void *malloc_checked(unsigned int);
+char *string_nconcat(char*, char*, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
