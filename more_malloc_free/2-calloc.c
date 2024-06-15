3311170a38c94ce21ae778e2677142d2d57c2bac
internashionalist <taylor.thames@atlasschool.com>
2024-06-15 19:08:50 +0000

    2.0 - base

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for array of nmemb elements of size bytes each\
+ * @nmemb: number of elements
+ * @size: size of elements
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
