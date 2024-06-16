44e8befc64f735e13f74f2ad1d0dc5ec286302a1
internashionalist <taylor.thames@atlasschool.com>
2024-06-16 18:35:29 +0000

    1.2 - measure strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,9 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int s1len;
+	unsigned int s2len;
+	
 	if (s1 == NULL) /* if NULL, treat as empty strings */
 	{
 		s1 = "";
@@ -23,4 +26,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	
+	for (s1len = 0; s1[s1len] != '\0'; s1len++) /* measure s1 */
+	{
+		;
+	}
+
+	for (s2len = 0; s2[s2len] != '\0'; s2len++) /* measure s2 */
+	{
+		;
+	}
+
+
