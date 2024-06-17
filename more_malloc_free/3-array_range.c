a504d7192297a25bfb02a0004eb621803160a931
internashionalist <taylor.thames@atlasschool.com>
2024-06-17 00:26:12 +0000

    3.0 - base

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * array_range - creates array of integers
+ * @min: min of range
+ * @max: max of range
+ *
+ * Return: pointer to new array
+ */
+
+int *array_range(int min, int max)
+{
