2a5cdf6b0837a82ef05ae246033178282000fa78
internashionalist <taylor.thames@atlasschool.com>
2024-06-17 12:05:16 -0500

    3.3 - looking good

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,3 +10,28 @@
 
 int *array_range(int min, int max)
 {
+    int *array;
+    int i;
+    int size;
+
+    if (min > max) /* if min > max */
+    {
+        return (NULL);
+    }
+
+    size = (max - min) + 1; /* contain all integers */
+
+    array = malloc(sizeof(int) * size); /* allocate */
+
+    if (array == NULL) /* if malloc fails */
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++) /* iterate through */
+    {
+        array[i] = min++; /* increment */
+    }
+
+    return (array);
+}
