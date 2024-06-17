43e8fc32c4488e42f8e5b98bbd4758e1fc281aeb
internashionalist <taylor.thames@atlasschool.com>
2024-06-17 00:18:09 +0000

    2.2 - cautiously optimistic

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _calloc - allocates memory for array of nmemb elements of size bytes each\
+ * _calloc - allocates memory for array of nmemb elements of size bytes each
  * @nmemb: number of elements
  * @size: size of elements
  *
@@ -10,12 +10,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+	void *memptr;
+	
 	if (nmemb == 0)
-	{
 		return (NULL);
-	}
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+
+	memptr = malloc(nmemb * size);
+
+	if (memptr == NULL)
+		return (NULL);
