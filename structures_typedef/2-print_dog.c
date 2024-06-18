fd314da63c1123573a2e4a18aa47e853d599220e
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-18 21:56:48 +0000

    2.0 - base

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints struct dog.
+ * @d: struct dog being printed.
+ *
+ * Return - void
+ */
+
+
+void print_dog(struct dog *d)
+{
