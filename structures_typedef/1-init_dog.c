026c5cf0dd9249c5767bd287f922459cbb3ac81f
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-18 12:02:34 +0000

    1.0 - base

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initializes struct dog
+ *
+ * @d: pointer to struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
