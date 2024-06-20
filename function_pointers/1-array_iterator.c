8e2e75f11efed100e536ef6ab235fcd980b25b87
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-20 05:55:28 +0000

    1.2

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,4 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action && array)
+	if (action && array) /* if both are not NULL */
+		for (i = 0; i < size; i++) /* iterate through array */
+			action(array[i]); /* apply to each element */
+}
