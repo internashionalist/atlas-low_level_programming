dfe0b848949e49c38a8a6268ec7d34b0ef4a0c5a
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-20 04:35:50 +0000

    0.3 - fixed header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 
 #include <stdlib.h>
 
-int _putchar(char c)
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cpm)(int));
