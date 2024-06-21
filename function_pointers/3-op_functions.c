f75f750eab9c3ae63147932c5bb77aca1e22aeb2
internashionalist <taylor.thames@atlasschool.com>
2024-06-21 04:01:37 +0000

    3.1 - op functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: first number added
+ * @b: second number added
+ *
+ * Return: addition result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - finds difference between a and b
+ * @a: first number
+ * @b: number subtracted
+ *
+ * Return: difference result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies a by b
+ * @a: first number multiplied
+ * @b: second number multiplied
+ *
+ * Return: multiplication result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ * @a: number divided
+ * @b: divisor
+ *
+ * Return: division result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds remainder of a / b
+ * @a: number divided
+ * @b: divisor
+ *
+ * Return: remainder of division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
