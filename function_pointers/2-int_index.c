602efb5d152e675e65706b6ac3e7647fae0e80df
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-21 02:19:18 +0000

    2.3 - almost there

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,10 +13,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	
 	if (array && cmp)
 	{
-		if (size <= 0)
+		for (i = 0; i < size; i++)
 		{
-			return (-1);
+			if (cmp(array[i]))
+				{
+					return (i);
+				}
 		}
-	
+	}
+}
