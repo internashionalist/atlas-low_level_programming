dec4a6917522b79c1069a422298c09c58718a143
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-21 10:31:54 +0000

    3.6 - main progress

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,5 +9,26 @@
  *	100 if attempted div or mod by 0, 0 on success
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int num1;
+	int num2;
+	int *operation (int, int);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	operation = get_op_func(operator)
+
+	if (argc != 4) /* if number of arguments wrong */
+	{
+		printf("Error\n");
+		exit(98); /* Error Code 98 */
+	}
+
+	/* need Error Code 99 if statement */
+
+	/* Error Code 100 addressed in get_op_func */
+
+	
