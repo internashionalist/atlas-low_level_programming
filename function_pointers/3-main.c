f49c1b81b95813fb83c68e1b8fbdfad5e8fb972f
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-21 10:09:47 +0000

    3.3 - main base, op functions 0 cases added

---
+++ function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+
+/**
+ * main - executes functions called
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 98 if argc incorrect, 99 if null operator,
+ *	100 if attempted div or mod by 0, 0 on success
+ */
+
+int main(int argc, char **argv)
+{
