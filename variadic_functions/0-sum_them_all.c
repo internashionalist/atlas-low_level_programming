ae2fea7bd9a72f9025e855dd53f522309bb1e468
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-22 08:01:15 +0000

    0.0 - base

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - finds sum of all parameters
+ * @n: numbers of parameters
+ *
+ * Return: sum of parameters, 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
