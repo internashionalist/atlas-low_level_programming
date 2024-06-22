8c5b83513ffa2f31799e07cf23f2df951aa3baa7
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-22 08:03:24 +0000

    1.0 - base

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers + \n
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
