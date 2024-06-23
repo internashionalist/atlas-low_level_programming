a83251d09463ba493bd74981d9b6a79e5f95a9fc
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-23 09:49:53 +0000

    2.1 - compiles, looks good

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,7 +2,7 @@
 
 /**
  * print_strings - prints strings + \n
- * @separator: string printed between
+ * @separator: string printed between strings
  * @n: number of strings passed
  *
  * Return: void
@@ -10,5 +10,38 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
+	va_list args;
 	unsigned int i;
+	char *strings;
+
+	if (n == '\0')
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		strings = va_arg(args, char *); /* retrieve strings */
+
+		if (strings == NULL) /* if a string is NULL */
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", strings);
+		}
+		
+		if (i < n - 1 && separator != NULL) /* if not last */
+                {
+                        printf("%s", separator); /* print sep */
+                }
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
