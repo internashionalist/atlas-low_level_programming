f86099ecf5b73de70cda2a6fb018d0fbfd9bbc4b
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-23 02:19:11 +0000

    1.1

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,3 +9,23 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list nums; /* init arg list */
+	unsigned int i; /* iterator */
+	int sum = 0;
+
+	if (n == 0) /* if no args */
+	{
+		return (0);
+	}
+
+	va_start(nums, n); /* inits var list with first arg n */
+
+	for (i = 0; i < n; i++) /* iterate through var args */
+	{
+		sum += va_arg(nums, int); /* retrieves next arg, adds to sum */
+	}
+
+	va_end(nums); /* clean up */
+
+	return (sum); /* return arg sum */
+}
