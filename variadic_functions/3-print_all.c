b5ac9417de323bf090088bad4d3d257a065eedf9
“internashionalist” <taylor.thames@atlaschool.com>
2024-06-23 17:17:42 +0000

    3.2 - first three cases

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -21,3 +21,19 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i]) 
 	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator,  va_arg(args, int));
+				break;
+			
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+
+
+
