00b0ab87748f930ca836a17e21daea417c19f5dd
internashionalist <taylor.thames@atlasschool.com>
2024-06-23 09:52:00 +0000

    3.0 - base

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * print_all - print anything
+ * @format: list of types of arguments passed
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
