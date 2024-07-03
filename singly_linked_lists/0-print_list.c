e1f1abb161c41e0254da400beca12d3153435419
internashionalist <taylor.thames@atlasschool.com>
2024-07-02 23:23:16 -0500

    0.6 - vscode fun

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -20,11 +20,10 @@ size_t print_list(const list_t *h)
 		}
 		else
         {
-
-		}    printf("[%d] %s\n", h->len, h->str);
-	}
+			printf("[%d] %s\n", h->len, h->str);
+		}
     node_count++;
     h = h->next;
-
+	}
     return (node_count);
-}
+}	
