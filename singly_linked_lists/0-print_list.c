e288389119aad93eb19f96020167c538d1a5108d
internashionalist <168384665+internashionalist@users.noreply.github.com>
2024-07-02 22:47:00 -0500

    0.3 - base

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ *
+ * @h: pointer to head of a list_t list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	int node_count = 0;
+
