e8aee7b938863487bbba30d467976a6325e190d3
internashionalist <taylor.thames@atlasschool.com>
2024-07-02 23:00:29 -0500

    0.5 - why cant i get betty to work on vscode

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,16 +10,21 @@
 
 size_t print_list(const list_t *h)
 {
-
 	int node_count = 0;
 
-    while (h != NULL)
-    {
+	while (h != NULL)
+	{
         if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+        {
 
-        node_count++;
-        h = h->next;
-    }
+		}    printf("[%d] %s\n", h->len, h->str);
+	}
+    node_count++;
+    h = h->next;
+
+    return (node_count);
+}
