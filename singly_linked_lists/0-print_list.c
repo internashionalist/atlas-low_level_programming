89fe9f42cd85c879deefaf729eb066135f1a67e1
internashionalist <taylor.thames@atlasschool.com>
2024-07-02 22:50:28 -0500

    0.3 - base

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,3 +13,13 @@ size_t print_list(const list_t *h)
 
 	int node_count = 0;
 
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+
+        node_count++;
+        h = h->next;
+    }
