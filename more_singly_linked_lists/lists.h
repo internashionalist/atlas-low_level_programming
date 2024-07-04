df64198dad60ec3d83e6b0deed4cf5f4acaf21d8
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 18:15:42 -0500

    header and README

---
+++ more_singly_linked_lists/lists.h
@@ -0,0 +1,34 @@
+#ifndef list_h
+#define list_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *);
+size_t listint_len(const listint_t *);
+listint_t *add_nodeint(listint_t **, const int);
+listint_t *add_nodeint_end(listint_t **, const int);
+void free_listint(listint_t *);
+void free_listint2(listint_t **);
+int pop_listint(listint_t **);
+listint_t *get_nodeint_at_index(listint_t *, unsigned int);
+int sum_listint(listint_t *);
+listint_t *insert_nodeint_at_index(listint_t **, unsigned int, int);
+int delete_nodeint_at_index(listint_t **, unsigned int);
+
+#endif
