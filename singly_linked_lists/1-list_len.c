493b4eedbf58b1ff881d3f5f42f9bd384b3ec03f
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 21:09:13 +0000

    1.0 base

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list_t list
+ * @h: head node
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
