a34853089e413ec852523258ce1ac472d1552c5f
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 17:29:21 -0500

    3.0 - base + variables

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a linked list_t list
+ * @head: head of list
+ * @str: str to be added to new node
+ *
+ * Return: address of new node or NULL on failure
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node; /* create new node */
+	list_t *last_node; /* create last node */
+	unsigned int len; /* length counter */
+	char *string_copy; /* duplicate of string */
+
+	
