e1f4c21a68ce31d20d1c5653e131307f48ad13ac
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 17:55:26 -0500

    4.1 - that was easy!

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,10 +2,20 @@
 
 /**
  * free_list - frees a linked list_t list
- * @head: head of list_t list
+ * @head: head of linked list
  *
  * Return: void
  */
 
 void free_list(list_t *head)
 {
+	list_t *temp_node; /* temp node to free */
+
+	while (head != NULL) /* iterate through list */
+	{
+		temp_node = head; /* set temp to head */
+		head = head->next; /* move head to next node */
+		free(temp_node->str); /* free string of temp */
+		free(temp_node); /* free temp */
+	}
+}
