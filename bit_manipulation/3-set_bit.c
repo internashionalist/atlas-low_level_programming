59dce9b9ce7f673e01b5e9519c791a22cb71b3b7
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 11:22:33 -0500

    3.0 - base

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit to '1' at a given index
+ * @n: number to be modified
+ * @index: index of bit being modified
+ *
+ * Return: 1 on success, -1 on failure
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
