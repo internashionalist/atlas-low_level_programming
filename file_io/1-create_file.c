efc6a81261ced1d96153b1ab9e3cb64f1997fd5e
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 16:13:36 -0500

    1.4 - file open, fail case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,9 @@ int create_file(const char *filename, char *text_content)
 	/* open file, create if it doesn't exist, write only, truncate */
 	descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (descriptor == -1) /* if open or any part fails */
+	if (descriptor == -1) /* if any part of that fails */
 	{
 		return (-1); /* return -1 */
 	}
+
+	
