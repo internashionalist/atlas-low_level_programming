f85f51d03f35b8dc193d018964a72e55620764fb
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 08:46:08 -0500

    10.0 - base

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index- deletes node of linked listint_t list where specified
+ * @head: head of linked listint_t list
+ * @index: position of node to be deleted
+ *
+ * Return: 1 if succeeded or -1 on failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
