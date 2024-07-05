4565feb7fdebf4d092392c31706f0dd3e7bcd4c5
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 11:47:13 -0500

    4.0 - base + copypaste from 2 & 3

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets value of bit to 0 at a given index
+ * @n: number to be modified
+ * @index: index of bit being modified
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8) /* if index greater than 64 */
+	{
+		return (-1); /* return -1 */
+	}
