921a960d90b9ad11f1bab5152b463f1622608e16
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 14:12:06 -0500

    4.1 - stupid ~

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -14,3 +14,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	{
 		return (-1); /* return -1 */
 	}
+
+	*n &= ~(1 << index); /* set bit to '0' at index */
+
+	return (1); /* return 1 */
+}
