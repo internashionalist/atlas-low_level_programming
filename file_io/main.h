63f312bd4d61c8deffe897626e2f261b9bd05044
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 15:06:27 -0500

    0.0 - alllll the libraries

--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
