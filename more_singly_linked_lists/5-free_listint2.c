a7acd3602e526d360d4606c4e2cbb171d5072b6f
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 21:54:10 -0500

    5.1 - free list, compiles

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,4 +9,19 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = NULL;
+	listint_t *temp_node = NULL; /* temporary node */
+
+	if (head == NULL) /* check if head is NULL */
+	{
+		return;
+	}
+
+	while (*head != NULL) /* iterate through list */
+	{
+		temp_node = *head; /* set temp_node to head */
+		*head = (*head)->next; /* move head to next node */
+		free(temp_node); /* free temp_node */
+	}
+
+	*head = NULL; /* set head to NULL */
+}
