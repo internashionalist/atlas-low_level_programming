14bc38bd11cf54fd0a8cc647ef5d987ae0aba9e3
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 17:37:15 -0500

    3.1 - it's a start

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,1 +1,12 @@
-THIS IS GOING TO SUCK. I CAN FEEL IT.
+#include "main.h"
+
+/**
+ * copy_file - copies the content of a file to another file
+ * @file_from: file to copy from
+ * @file_to: file to copy to
+ *
+ * Return: 1 on success
+ */
+
+int copy_file(const char *file_from, const char *file_to)
+{}
--- file_io/main.h
+++ file_io/main.h
@@ -13,6 +13,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int cp_file(const char *file_from, const char *file_to);
+int copy_file(const char *file_from, const char *file_to);
 
 #endif
