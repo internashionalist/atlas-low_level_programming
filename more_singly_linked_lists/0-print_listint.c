fd3566480184f435604e61aa5127bf2ae2173162
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 20:36:16 -0500

    0.0 - base

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: head of linked listint_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
