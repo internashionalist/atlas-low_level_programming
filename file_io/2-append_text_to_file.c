fea6bc4f66233e23b25ead011280c7432287eba8
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 17:00:04 -0500

    2.3 - nearly identical to task 1

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at end of file
  * @filename: name of file
- * @text_content: content to append
+ * @text_content: NULL terminated string to add at end of file
  *
  * Return: 1 on success or -1 on failure
  */
@@ -15,6 +15,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	int content_length = 0; /* length of text_content */
 
 	if (filename == NULL) /* if filename NULL */
+	{
+		return (-1); /* return -1 */
+	}
+	/* open file, DO NOT CREATE, write only, append */
+	descriptor = open(filename, O_WRONLY | O_APPEND);
+
+	if (descriptor == -1) /* if any part of open fails */
 	{
 		return (-1); /* return -1 */
 	}
