1601dc95fce946567a5104d2b9a3b7eb8cd38dbe
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 21:45:07 -0500

    4.1 - freeeeeee

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -10,3 +10,11 @@
 void free_listint(listint_t *head)
 {
 	listint_t *temp_node; /* temporary node */
+
+	while (head != NULL) /* iterate through list */
+	{
+		temp_node = head; /* set temp_node to head */
+		head = head->next; /* move head to next node */
+		free(temp_node); /* free temp_node */
+	}
+}
