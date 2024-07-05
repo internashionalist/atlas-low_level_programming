9781ade81ec4775f903c3459430e9acc01557773
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 16:53:18 -0500

    2.0 - base

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at end of file
+ * @filename: name of file
+ * @text_content: content to append
+ *
+ * Return: 1 on success or -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
