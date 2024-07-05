5d3de4c1d104ece79705971beb8efc9f11feb9b0
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 09:45:08 -0500

    1.0 - main

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: unsigned long int being printed
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	
