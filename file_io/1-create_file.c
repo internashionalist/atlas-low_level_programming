5b301439f10841b4948416b798f8a9be1912c298
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 16:09:26 -0500

    1.3 - on the right track

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,3 +17,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1); /* return -1 */
 	}
+	/* open file, create if it doesn't exist, write only, truncate */
+	descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (descriptor == -1) /* if open or any part fails */
+	{
+		return (-1); /* return -1 */
+	}
