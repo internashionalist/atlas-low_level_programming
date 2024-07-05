2f34c7063155eec762b1f468fc72ed407bc158c9
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 16:01:07 -0500

    1.2 - variables - may need more

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,6 +10,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int descriptor;
+	int num_write;
+
 	if (filename == NULL) /* if filename NULL */
 	{
 		return (-1); /* return -1 */
