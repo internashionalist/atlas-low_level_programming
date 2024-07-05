856a899a47c7e2c71328d0d3d27d3f7d99e818b0
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 18:00:55 -0500

    1.10 - removed WRONLY

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1); /* return -1 */
 	}
 	/* open file, create if it doesn't exist, write only, truncate */
-	descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	descriptor = open(filename, O_CREAT | O_TRUNC, 0600);
 
 	if (descriptor == -1) /* if any part of that fails */
 	{
