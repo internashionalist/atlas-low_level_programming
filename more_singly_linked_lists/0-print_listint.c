ece0b2fca16671509481e045b39bbf82ff4c2ccd
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 20:42:44 -0500

    0.1 - should be good

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,3 +9,16 @@
 
 size_t print_listint(const listint_t *h)
 {
+	size_t node_count = 0; /* node counter */
+
+	
+
+	while (h != NULL) /* iterate through list */
+	{
+		printf("%d\n", h->n); /* print node value */
+		node_count++; /* increment node counter */
+		h = h->next; /* move to next node */
+	}
+
+	return (node_count); /* return number of nodes */
+}
