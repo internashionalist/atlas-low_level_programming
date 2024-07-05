122f5283a338ffdd6dbccbe12a415896ddf15b4e
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 14:40:55 -0500

    5.4 - betty, fixed formatting

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -14,13 +14,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int bit_count = 0;
 	unsigned int iterator = 0;
 
-	for (iterator = 0; iterator < (sizeof(n) * 8); iterator++) /* iterate through bits */
+	for (iterator = 0; iterator < (sizeof(n) * 8); iterator++) /* iterate through */
+	{
+		if (((n >> iterator) & 1) != ((m >> iterator) & 1)) /* if different */
 		{
-			if (((n >> iterator) & 1) != ((m >> iterator) & 1)) /* if different bits */
-			{
-				bit_count++; /* increment bit_count */
-			}
+			bit_count++; /* increment bit_count */
 		}
+	}
 
 	return (bit_count); /* return count of bits needed */
 }
