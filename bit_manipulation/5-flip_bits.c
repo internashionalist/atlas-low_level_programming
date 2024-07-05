be58fcaad1aec74ccb771cdd2e023f85e045d96b
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 14:39:19 -0500

    5.3 - removed superfluous while loop, compiles!

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -14,10 +14,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int bit_count = 0;
 	unsigned int iterator = 0;
 
-	while (iterator < (sizeof(n) * 8)) /* while less than 64 bits */
-	{
-		for (iterator = 0; iterator < (sizeof(n) * 8); iterator++)
+	for (iterator = 0; iterator < (sizeof(n) * 8); iterator++) /* iterate through bits */
 		{
 			if (((n >> iterator) & 1) != ((m >> iterator) & 1)) /* if different bits */
-			
+			{
+				bit_count++; /* increment bit_count */
+			}
 		}
+
+	return (bit_count); /* return count of bits needed */
+}
