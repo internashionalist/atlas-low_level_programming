57af4e6adfdc4719e3ce61c807f724bb7929da46
internashionalist <taylor.thames@atlasschool.com>
2024-07-04 21:49:40 -0500

    5.0 - base

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head of listint_t list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL;
