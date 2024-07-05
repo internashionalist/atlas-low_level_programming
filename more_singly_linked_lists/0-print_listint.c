048a5bc2168fe80c290ba50b92fa89e48501ca73
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 01:48:37 +0000

    0.2 - betty

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -11,8 +11,6 @@ size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 0; /* node counter */
 
-	
-
 	while (h != NULL) /* iterate through list */
 	{
 		printf("%d\n", h->n); /* print node value */
