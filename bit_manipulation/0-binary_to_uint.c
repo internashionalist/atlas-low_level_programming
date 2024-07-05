b871c11bdd12919fac39e8904cd7f7389188a963
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 09:31:12 -0500

    0.0 - base

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,8 @@
+/**
+ * binary_to_uint - converts binary number to unsigned int
+ * @b: binary number as a string
+ *
+ * Return: converted binary number as unsigned int or 0 if b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
