b71f79565364a8272f452df7fb9343d6a8eee601
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 16:57:22 -0500

    2.2 - filename null case

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,3 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	int descriptor; /* file descriptor */
 	int num_write; /* number of characters written */
 	int content_length = 0; /* length of text_content */
+
+	if (filename == NULL) /* if filename NULL */
+	{
+		return (-1); /* return -1 */
+	}
