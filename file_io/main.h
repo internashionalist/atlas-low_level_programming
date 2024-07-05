5c393a20ef670cbf1be7566f4666074acb3f07e6
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 15:04:33 -0500

    header, README

---
+++ file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int cp_file(const char *file_from, const char *file_to);
+
+#endif
