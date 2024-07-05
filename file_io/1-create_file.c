f15db1b7f5f60a98aa887222805fb8281e793669
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 15:53:18 -0500

    1.0 - base

---
+++ file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * create_file - creates file
+ * @filename: name of file being created
+ * @text_content: content of file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
