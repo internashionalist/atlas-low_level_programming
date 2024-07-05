6a93481bd49d890d71208ff339ac1a7ae90ce717
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 08:42:59 -0500

    9.6 - KERNEL style???

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,28 +16,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current_node; /* current node */
 
 	new_node = malloc(sizeof(listint_t)); /* allocate memory for new node */
-
-	if (head == NULL) /* if list is empty */
+	if (head == NULL || new_node == NULL) /* if list is empty or malloc fails */
 	{
 		return (NULL); /* return NULL */
 	}
-
-	if (new_node == NULL) /* check if memory allocation failed */
-	{
-		return (NULL); /* return NULL on failure */
-	}
-
 	new_node->n = n; /* set value of new node */
-
 	if (idx == 0) /* if new node is to be added at beginning */
 	{
 		new_node->next = *head; /* set next of new node to head */
 		*head = new_node; /* set head to new node */
 		return (new_node); /* return address of new node */
 	}
-
 	current_node = *head; /* set current_node to head */
-
 	while (index_counter < idx - 1) /* iterate through list until index */
 	{
 		if (current_node == NULL) /* if current_node is NULL */
@@ -45,13 +35,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			free(new_node); /* free new_node */
 			return (NULL); /* return NULL */
 		}
-
 		current_node = current_node->next; /* move to next node */
 		index_counter++; /* increment index_counter */
 	}
-
-	new_node->next = current_node->next; /* set next of new_node to current_node's next */
+	new_node->next = current_node->next; /* set next of current_node */
 	current_node->next = new_node; /* set next of current_node to new_node */
-
 	return (new_node); /* return address of new node */
 }
