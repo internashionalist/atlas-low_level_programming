796702faafa8178e1204137719ae6aa227dd0009
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 15:09:16 -0500

    0.1 - base - this is going to suck

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * read_textfile- reads a text file and prints it to the POSIX standard output.
+ * @filename: name of the file to be read
+ * @letters: number of chars being read and printed
+ *
+ * Return: number of chars printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
