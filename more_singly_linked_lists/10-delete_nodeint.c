575f53992af4a7669cabd91b44df521f37573122
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 08:53:37 -0500

    9.1 - null cases, variables

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -10,3 +10,11 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int index_counter; /* index counter */
+	listint_t *current_node; /* current node */
+	listint_t *temp_node; /* temporary node */
+
+	if (head == NULL || *head == NULL) /* if head is NULL or list empty */
+	{
+		return (-1); /* return -1 */
+	}
