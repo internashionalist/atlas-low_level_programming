bcd6e805011671cf5491d471cbee1edca85fa2a9
internashionalist <taylor.thames@atlasschool.com>
2024-07-06 09:08:13 -0500

    2.10 - reversing changes

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,8 +25,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1); /* return -1 - failure */
 	}
+
+	if (text_content == NULL) /* if text_content is NULL */
+	{
+		close(descriptor); /* close file */
+		return (1); /* return 1 - success */
+	}
 	/* find length of text_content */
-	while (text_content != NULL ||text_content[content_length] != '\0')
+	while (text_content[content_length] != '\0')
 	{
 		content_length++; /* increment counter */
 	}
