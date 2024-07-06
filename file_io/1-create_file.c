09befd19d7eb6f8b77315899dbf4151695226161
internashionalist <taylor.thames@atlasschool.com>
2024-07-06 06:01:22 -0500

    1.14 - made permissions flags

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,13 +13,14 @@ int create_file(const char *filename, char *text_content)
 	int descriptor; /* file descriptor */
 	int num_write; /* number of chars written */
 	int content_length = 0; /* length of text_content */
+	mode_t permissions = S_IRUSR | S_IWUSR; /* permissions for new file */
 
 	if (filename == NULL) /* if filename NULL */
 	{
 		return (-1); /* return -1 */
 	}
 	/* open file, create if it doesn't exist, readwrite, truncate */
-	descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, permissions);
 
 	if (descriptor == -1) /* if any part of that fails */
 	{
