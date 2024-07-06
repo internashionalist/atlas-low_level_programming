b6236d7b021d26f0b935465d9aec44d6616d268f
internashionalist <taylor.thames@atlasschool.com>
2024-07-05 23:48:18 -0500

    3.5 - so many error cases. why am I coding right now. I ama mazing at karaoke.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1); /* return -1 */
 	}
-	/* open file, create if it doesn't exist, write only, truncate */
+	/* open file, create if it doesn't exist, readwrite, truncate */
 	descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (descriptor == -1) /* if any part of that fails */
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,14 +14,61 @@ int copy_file(const char *file_from, const char *file_to)
 	int num_read, num_write; /* number of characters read and written */
 	char buffer[1024]; /* buffer to hold characters */
 
-	if (file_from == NULL || file_to == NULL) /* if either file NULL */
-	{
-		return (-1); /* return -1 */
-	}
-
-	descriptor_from = open(file_from, O_RDONLY); /* open file_from - RDONLY??? */
+	/* open file_from, DO NOT CREATE, read only */
+	descriptor_from = open(file_from, O_RDONLY);
 
 	if (descriptor_from == -1) /* if open fails */
 	{
-		return (-1); /* return -1 */
+		/* error message for open failure */
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98); /* exit with code 98 */
 	}
+
+	/* open file_to, create if it doesn't exist, readwrite, truncate */
+	descriptor_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (descriptor_to == -1) /* if open fails */
+	{
+		/* error message for open failure */
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99); /* exit with code 99 */
+	}
+
+	/* read from file_from into buffer */
+	num_read = read(descriptor_from, buffer, 1024);
+
+	if (num_read == -1) /* if read fails */
+	{
+		/* error message for read failure */
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98); /* exit with code 98 */
+	}
+
+	/* write from buffer to file_to */
+	num_write = write(descriptor_to, buffer, num_read);
+
+	if (num_write == -1) /* if write fails */
+	{
+		/* error message for write failure */
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99); /* exit with code 99 */
+	}
+
+	/* close file_from */
+	if (close(descriptor_from) == -1)
+	{
+		/* error for close failure */
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", descriptor_from);
+		exit(100); /* exit with code 100 */
+	}
+
+	/* close file_to */
+	if (close(descriptor_to) == -1)
+	{
+		/* error for close failure */
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", descriptor_to);
+		exit(100); /* exit with code 100 */
+	}
+
+	return (1); /* return 1 on success */
+}
