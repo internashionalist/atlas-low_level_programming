eefbf9580454f994be6e73598b93d8ea474987a4
internashionalist <taylor.thames@atlasschool.com>
2024-07-07 03:32:39 -0500

    100.0 - guess I know endianness now

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks... <looks at notes> ..endianness?
+ * 
+ * Return: 0 if big, 1 if little
+ */
+
+int get_endianness(void)
+{
+	unsigned int n; /* checks endianness */
+	char *c; /* pointer to n's bytes */
+
+	n = 1; /* set n to 1 */
+	c = (char *)&n; /* set c to n's bytes */
+
+	if (*c) /* if first byte of n is 1 */
+	{
+		return (1); /* little endian */
+	}
+
+	return (0); /* otherwise big endian */
+}
