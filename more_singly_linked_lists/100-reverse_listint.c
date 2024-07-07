af37706fb00590ad3404ba5a48b7202b8478a79e
internashionalist <taylor.thames@atlasschool.com>
2024-07-07 04:24:52 -0500

    100.1 - looks good

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -11,5 +11,16 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev_node = NULL; /* previous node */
 	listint_t *next_node = NULL; /* next node */
+	listint_t *current_node = *head; /* current node */
 
-	
+	while (current_node != NULL) /* iterate through list */
+	{
+		next_node = current_node->next; /* set next node */
+		current_node->next = prev_node; /* set next of current to prev */
+		prev_node = current_node; /* set prev to current */
+		current_node = next_node; /* set current to next */
+	}
+
+	*head = prev_node; /* set head to prev */
+	return (*head); /* return new head */
+}
