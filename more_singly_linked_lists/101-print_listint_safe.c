a290d93d24ad0dd9cc7890c656b6a5886c79bb2d
internashionalist <taylor.thames@atlasschool.com>
2024-07-08 07:01:00 -0500

    101.4 - AI use, but I learned a lot

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,19 @@
 #include "lists.h"
 
 /**
+ * struct listint_s - singly linked list
+ * @n: int
+ * @next: points to the next node
+ *
+ * Description: node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
  * print_listint_safe - prints a linked list_int list
  * @head: first node of the list
  *
@@ -9,24 +22,54 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *tortoise; /* slow pointer */
-	listint_t *hare; /* fast pointer */
-	size_t node_count = 1; /* counter */
+	listint_t *tortoise = head; /* slow pointer */
+	listint_t *hare = head; /* fast pointer */
+	size_t node_count = 0; /* counter */
 
-	if (head == NULL) /* check for NULL */
+	while (tortoise && hare && hare->next) /* while tortoise and hare exist */
 	{
-		exit(98); /* exit status 98 */
-	}
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n); /* print tortoise */
+		node_count++; /* increment node count */
 
-	tortoise = (listint_t *)head; /* set tortoise to head */
-	hare = head->next; /* set hare to next node */
+		tortoise = tortoise->next; /* move tortoise to next node */
+		hare = hare->next->next; /* move hare to next NEXT node */
+		
+		if (tortoise == hare) /* if tortoise and hare meet */
+		{
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			node_count++; /* print tortoise and increment node count */
 
-	while (hare) /* iterate through list */
-	{
-		if (tortoise == hare) /* if tortoise catches hare */
-		{	/* print tortoise */
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n)
+			tortoise = tortoise->next; /* move tortoise to next node */
+
+			while (tortoise != hare) /* while tortoise and hare don't meet */
+			{
+				printf("[%p] %d\n", (void *)tortoise, tortoise->n); /* print tortoise */
+				node_count++; /* increment node count */
+				tortoise = tortoise->next; /* move tortoise to next node */
+			}
+
+			printf("-> [%p] %d\n", (void *)hare, hare->n); /* print hare */
 			return (node_count); /* return number of nodes */
 		}
-	
+	}
+
+	/* if there is no loop */
+	while (head) /* while head exists */
+	{
+		printf("[%p] %d\n", (void *)head, head->n); /* print head */
+		node_count++; /* increment node count */
+		head = head->next; /* move head to next node */
+	}
+
+	return (node_count); /* return number of nodes */
+}
+
+/**
+ * main - creates linked list and prints it
+ * 
+ * Return: 0 on success
+ */
+
+int main(void)
+
 
