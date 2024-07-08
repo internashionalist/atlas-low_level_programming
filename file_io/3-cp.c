c8a688c790d4060b3cec8627585d3883c3d9f0e6
internashionalist <taylor.thames@atlasschool.com>
2024-07-08 17:46:45 -0500

    3.33 - hopefully betty likes this

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,59 +15,42 @@ int main(int argc, char *argv[])
 	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3) /* if number of arguments is incorrect */
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); /* print error message and exit with code 97 */
-	}
 
 	file_from = open(argv[1], O_RDONLY); /* open file_from - read only */
-
 	if (file_from == -1) /* if open fails */
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); /* print error message and exit with code 98 */
-	}
-	/* open file_to - create, write only, truncate */
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
 
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions); /* open file_to */
 	if (file_to == -1) /* if open fails */
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); /* print error message and exit with code 99 */
-	}
-	/* read data in file_from into buffer */
-	while ((n_read = read(file_from, buffer, 1024)) > 0)
+
+	while ((n_read = read(file_from, buffer, 1024)) > 0) /* read file_from into buffer */
 	{
 		n_write = write(file_to, buffer, n_read); /* write data to file_to */
-
 		if (n_write == -1) /* if write fails */
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(file_from);
 			close(file_to);
 			exit(99); /* print error message and exit with code 99 */
-		}
 	}
 
 	if (n_read == -1) /* if read fails */
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(file_from); /* close file_from */
 		close(file_to); /* close file_to */
 		exit(98); /* print error message and exit with code 98 */
-	}
 
 	if (close(file_from) == -1) /* if file_from close fails */
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100); /* print error message and exit with code 100 */
-	}
 
 	if (close(file_to) == -1) /* if file_to close fails */
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100); /* print error message and exit with code 100 */
-	}
 
 	return (0); /* return 0 on success */
 }
