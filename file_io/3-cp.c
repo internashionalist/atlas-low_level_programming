cb31b4b2154d3f0a1d0e4cc8a162bb6f21b0b026
internashionalist <taylor.thames@atlasschool.com>
2024-07-08 18:02:48 -0500

    3.40 - formatting

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -21,12 +21,13 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY); /* open file_from - read only */
+
 	if (file_from == -1) /* if open fails */
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	/* open file_to, create if it doesn't exist, readwrite, truncate */
+	/* open file_to, create if it doesn't exist, readwrite, truncate, permissions */
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permissions);
 
 	if (file_to == -1) /* if open fails */
