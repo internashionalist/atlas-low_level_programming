b6abb83db32131540de72a9e6bde1938c609844c
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 02:57:19 -0500

    changed everything to the variable I actually first chose, added * to line 27, added malloc to compile

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,14 +9,25 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	
 	dlistint_t *new_head; /* pointer to new head node */
 
-	new_node->n = n; /* set value of new node */
-	new_node->prev = NULL; /* set prev of new node to NULL */
-	new_node->next = *head; /* set next of new node to current head */
-
-	if (head != NULL) /* if list is not empty */
+	new_head = malloc(sizeof(dlistint_t)); /* malloc for new head */
+	if (new_head == NULL) /* if malloc fails */
 	{
-		(*head)->prev = new_node; /* set prev of current head to new node */
+		return (NULL); /* return NULL */
 	}
-	
+
+	new_head->n = n; /* set value of new node */
+	new_head->prev = NULL; /* set prev of new node to NULL */
+	new_head->next = *head; /* set next of new node to current head */
+
+	if (*head != NULL) /* if list is not empty */
+	{
+		(*head)->prev = new_head; /* set prev of current head to new node */
+	}
+
+	*head = new_head; /* set head to new node */
+
+	return (new_head); /* return address of new node */
+}
