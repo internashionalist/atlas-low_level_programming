166176bd6787ebbd054988af32ea1e041de999b3
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 03:21:27 -0500

    3.3 - traverse list to current tail

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
        dlistint_t *new_tail; /* pointer to new tail node */
 
-       if (head == NULL) /* if list is empty */
+       if (head == NULL) /* check if head is NULL */
        {
               return (NULL); /* return NULL */
        }
@@ -23,3 +23,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
               return (NULL); /* return NULL */
        }
 
+       new_tail->n = n; /* set value of new tail */
+       new_tail->next = NULL; /* set next of new tail to NULL */
+       
+
+       current_node = *head; /* start at head of list */
+       while (current_node->next != NULL) /* traverse list to tail */
+       {
+              current_node = current_node->next; /* move to next node */
+       }
