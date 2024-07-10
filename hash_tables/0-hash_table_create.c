e157719bd4b688dcb6b74ef2988a7dfbf1dd4efd
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 13:35:48 -0500

    0.6 - betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new_table->size = size; /* set size of new_table to.. uh.. size */
 
-	new_table->array = malloc(sizeof(hash_node_t *) * size); /* malloc for array */
+	new_table->array = malloc(sizeof(hash_node_t *) * size); /* malloc array */
 	if (new_table->array == NULL) /* if malloc fails */
 		return (NULL); /* return NULL - failure */
 
