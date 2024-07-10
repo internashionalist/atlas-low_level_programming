e5129ccbd465161f437b5cf6be2336efa1163a29
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 06:07:29 -0500

    7.1 - starting with all the variables I think I'll need

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,3 +11,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node; /* pointer to new inserted node */
+	dlistint_t *current_node; /* pointer to current node */
+	unsigned int index_count = 0; /* index checker, set to 0 */
+	
