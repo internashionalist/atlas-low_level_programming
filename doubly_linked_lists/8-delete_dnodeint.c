8f107984ce2fcfeaad59e844577ad5f5ea306137
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 08:55:16 -0500

    8.0 - base

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index- deletes the node at given index
+ * @head: pointer to pointer to head of list
+ * @index: index of node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
