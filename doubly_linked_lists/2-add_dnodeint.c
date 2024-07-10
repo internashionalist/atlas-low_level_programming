e66138266691afb6a32646e5e6abf8cae37f4948
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 02:58:27 -0500

    2.3 - betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
+
 	dlistint_t *new_head; /* pointer to new head node */
 
 	new_head = malloc(sizeof(dlistint_t)); /* malloc for new head */
