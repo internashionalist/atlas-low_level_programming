7cebc4d786054c870132d9364b4807ffdbf24ff4
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 02:32:51 -0500

    1.1 - just copypasted from task 0 and took out printf?

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -9,3 +9,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	int node_count = 0; /* node counter */
+
+	while (h != NULL) /* iterate through list until NULL */
+	{
+		node_count++; /* increment counter by 1 */
+		h = h->next; /* move to next node in list */
+	}
+
+	return (node_count); /* return number of nodes */
+}
