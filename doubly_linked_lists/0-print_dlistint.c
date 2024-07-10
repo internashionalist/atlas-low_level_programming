d5d0045b24d22517dbb6726c6543387b0c32c8e1
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 02:10:29 -0500

    0.0 - base

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of dlistint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
