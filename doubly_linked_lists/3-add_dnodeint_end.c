26a3ead891fa90b4c8e6517abff02514f7c35fdf
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 03:54:04 -0500

    3.6 - stupid VSCode, forgot to set to tabs, betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,43 +10,43 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-       dlistint_t *new_tail; /* pointer to new tail node */
-       dlistint_t *current_node; /* pointer to current node */
+	dlistint_t *new_tail; /* pointer to new tail node */
+	dlistint_t *current_node; /* pointer to current node */
 
-       if (head == NULL) /* check if head is NULL */
-       {
-              return (NULL); /* return NULL */
-       }
+	if (head == NULL) /* check if head is NULL */
+	{
+		return (NULL); /* return NULL */
+	}
 
-       new_tail = malloc(sizeof(dlistint_t)); /* malloc for new tail */
-       if (new_tail == NULL) /* if malloc fails */
-       {
-              return (NULL); /* return NULL */
-       }
+	new_tail = malloc(sizeof(dlistint_t)); /* malloc for new tail */
+	if (new_tail == NULL) /* if malloc fails */
+	{
+		return (NULL); /* return NULL */
+	}
 
-       new_tail->n = n; /* set value of new tail */
-       new_tail->next = NULL; /* set next of new tail to NULL */
+	new_tail->n = n; /* set value of new tail */
+	new_tail->next = NULL; /* set next of new tail to NULL */
 
-       if (*head == NULL) /* if list is empty */
-       {
-              new_tail->prev = NULL; /* set prev of new tail to NULL */
-              new_tail->next = NULL; /* set next of new tail to NULL */
-              *head = new_tail; /* set pointer to head to new tail */
-       }
+	if (*head == NULL) /* if list is empty */
+	{
+		new_tail->prev = NULL; /* prev of new tail to NULL */
+		new_tail->next = NULL; /* next of new tail to NULL */
+		*head = new_tail; /* set pointer to head to new tail */
+	}
 
-       else /* if list is not empty */
-       {
-              current_node = *head; /* set current node to head */
+	else /* if list is not empty */
+	{
+		current_node = *head; /* set current node to head */
 
-              while (current_node->next != NULL) /* traverse list until last node */
-              {
-                     current_node = current_node->next; /* move to next node */
-              }
+		while (current_node->next != NULL) /* traverse list until NULL */
+		{
+			current_node = current_node->next; /* move to next node */
+		}
 
-              current_node->next = new_tail; /* set next of current node to new tail */
-              new_tail->prev = current_node; /* set prev of new tail to current node */
-              new_tail->next = NULL; /* set next of new tail to NULL */
-       }
+		current_node->next = new_tail; /* next of current node to new tail */
+		new_tail->prev = current_node; /* prev of new tail to current node */
+		new_tail->next = NULL; /* set next of new tail to NULL */
+	}
 
-       return (new_tail); /* return address of new tail */
+	return (new_tail); /* return address of new tail */
 }
