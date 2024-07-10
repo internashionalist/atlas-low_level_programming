02e13d60785061dcc2fbe9fb9766eb815a8ff1eb
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 05:47:50 -0500

    6.1 - added sum variable, compiles and runs

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -11,9 +11,18 @@
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *current_node = head; /* init traversal pointer */
+	int sum = 0; /* init sum, set to 0 */
 
 	if (head == NULL) /* if list is empty */
 	{
 		return (0); /* return 0 */
 	}
-	
+
+	while (current_node != NULL) /* traverse list until NULL */
+	{
+		sum += current_node->n; /* add data of current node to sum */
+		current_node = current_node->next; /* move to next node */
+	}
+
+	return (sum); /* return sum of all data */
+}
