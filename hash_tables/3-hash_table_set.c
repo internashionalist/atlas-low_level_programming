bd37a3718616669457a821ec64e233cee6a59688
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 17:54:45 -0500

    3.1 - init every pointer and variable I can think of

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,7 @@
 #include "hash_tables.h"
+#include "0-hash_table_create.c"
+#include "1-djb2.c"
+#include "2-key_index.c"
 
 /**
  * hash_table_set - adds an element to hash table
@@ -12,3 +15,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new_node = NULL; /* init ptr to hash_node_t struct, set to NULL */
+	hash_node_t *current_node = NULL; /* init traversal ptr, set to NULL */
+	unsigned long int index = 0; /* init index for hash table array, set to 0 */
+	char *duplicate = NULL; /* init ptr to duplicated value, set to NULL */
