0ae083fe04d39c326f3ae52959e4f188957a334d
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 11:30:24 -0500

    0.0 - base

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
