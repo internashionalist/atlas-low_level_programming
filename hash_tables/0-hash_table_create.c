1450544239f26d0830549ffc66b86dd3060bdb71
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 13:35:22 -0500

    0.4 - betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,16 +9,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL; /* init ptr to hash_table_t struct, set to NULL */
+	hash_table_t *new_table = NULL; /* init ptr to hash_table_t, set to NULL */
 	unsigned long int iterator = 0; /* init iterator, set to 0 */
 
-	new_table = malloc(sizeof(hash_table_t)); /* malloc for hash_table_t struct */
+	new_table = malloc(sizeof(hash_table_t)); /* malloc for struct */
 	if (new_table == NULL) /* if malloc fails */
 		return (NULL); /* return NULL - failure */
 
 	new_table->size = size; /* set size of new_table to.. uh.. size */
 
-	new_table->array = malloc(sizeof(hash_node_t *) * size); /* malloc for array ptrs */
+	new_table->array = malloc(sizeof(hash_node_t *) * size); /* malloc for array */
 	if (new_table->array == NULL) /* if malloc fails */
 		return (NULL); /* return NULL - failure */
 
