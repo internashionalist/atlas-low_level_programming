1dd8ebae6fe3be549f4fe616460e9e96312cedf7
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 03:37:27 -0500

    3.4 - initialized *current_node, had seg fault, working on new if else

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,6 +11,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
        dlistint_t *new_tail; /* pointer to new tail node */
+       dlistint_t *current_node; /* pointer to current node */
 
        if (head == NULL) /* check if head is NULL */
        {
@@ -25,10 +26,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
        new_tail->n = n; /* set value of new tail */
        new_tail->next = NULL; /* set next of new tail to NULL */
-       
 
-       current_node = *head; /* start at head of list */
-       while (current_node->next != NULL) /* traverse list to tail */
+       if (*head == NULL) /* if list is empty */
        {
-              current_node = current_node->next; /* move to next node */
+              new_tail->prev = NULL; /* set prev of new tail to NULL */
+              new_tail->next = NULL; /* set next of new tail to NULL */
+              *head = new_tail; /* set head to new tail */
        }
+
+       else /* if list is not empty */
+       {
+              current_node = *head; /* set current node to head */
+
+         
