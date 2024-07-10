6c2ad76a0ee46661bd83fe8b0f915022f9f3bba8
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 02:31:08 -0500

    1.0 - base

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements in a doubly linked list
+ * @h: pointer to head of list
+ *
+ * Return: number of elements in list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
