e712c6e78556d0de706e8a0323349771518333e4
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 13:14:33 -0500

    0.2 - malloc for struct and array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,6 +13,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int iterator = 0; /* init iterator, set to 0 */
 
 	new_table = malloc(sizeof(hash_table_t)); /* malloc for hash_table_t struct */
+	if (new_table == NULL) /* if malloc fails */
+		return (NULL); /* return NULL - failure */
 
-
-	
+	new_table->size = size; /* set size of new_table to.. uh.. size */
+	new_table->array = malloc(sizeof(hash_node_t *) * size); /* malloc for array ptrs */
+	if (new_table->array == NULL) /* if malloc fails */
+		return (NULL); /* return NULL - failure */
