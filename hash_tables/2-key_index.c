b536a8c638ffd2056ec943bda93177ca8960204c
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 17:14:37 -0500

    2.2 - compiles, runs

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,12 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * key_index - returns index of a key
+ * key_index - returns index of a key in hash table array
  * @key: key
- * @size: size of the hash table array
+ * @size: size of hash table array
  *
  * Return: index of the key in hash table array
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int index = hash_djb2(key); /* convert key to integer hash value */
+
+	return (index % size); /* index = hash value % size of array */
+}
