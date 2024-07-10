1bd42c788fb8fe5c60eaeb29c6b8a82e3f48146c
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 10:14:05 -0500

    8.2 - prev/next lines are the worst

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,10 +14,31 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int index_iterator = 0; /* init iterator, set to 0 */
 
 	if (*head == NULL) /* if list is empty */
-	{
 		return (-1); /* return -1 on failure */
+
+	if (index == 0) /* if index is at head */
+	{
+		*head = current_node->next; /* set head to next node */
+		if (current_node->next != NULL) /* if next node is not NULL */
+			current_node->next->prev = NULL; /* set prev of next node to NULL */
+
+		free(current_node); /* free memory of old head */
+		return (1); /* return 1 on success */
 	}
 
-	
+	while (current_node != NULL && index_iterator < index) /* traverse to node before index */
+	{
+		current_node = current_node->next; /* move to next node */
+		index_iterator++; /* increment iterator */
+	}
 
-	
+	if (current_node == NULL) /* if index is out of range */
+		return (-1); /* return -1 on failure */
+
+	current_node->prev->next = current_node->next; /* I can't stand these lines */
+	if (current_node->next != NULL) /* if not tail node */
+		current_node->next->prev = current_node->prev; /* seriously this took FOREVER */
+
+	return (1); /* SUCCESS */
+
+}
