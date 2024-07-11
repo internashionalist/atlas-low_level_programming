a2e7a4698afdca910e25d0abdb7e07c7de0d1599
internashionalist <taylor.thames@atlasschool.com>
2024-07-11 16:45:21 -0500

    4.2 - trick was to use task 2 function! compiles, runs

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,3 +10,20 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	int index; /* index of key in array */
+	hash_node_t *current_node; /* traversal ptr */
+
+	if (ht == NULL || key == NULL) /* if hash table or key is NULL */
+		return (NULL); /* return NULL */
+
+	index = key_index((unsigned char *)key, ht->size); /* FUNCTION - TASK 2 */
+
+	current_node = ht->array[index]; /* set traversal ptr to head of list */
+	while (current_node != NULL) /* traverse linked list at index */
+	{
+		if (strcmp(current_node->key, key) == 0) /* if key is found */
+			return (current_node->value); /* return value */
+		current_node = current_node->next; /*  or move to next node */
+	}
+	return (NULL); /* return NULL if key not found */
+}
