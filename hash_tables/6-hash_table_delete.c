1d181f39b5470b260156e4ee3891132ffc35528f
internashionalist <taylor.thames@atlasschool.com>
2024-07-11 18:30:21 -0500

    6.4 - NULLified array and size before freeing?

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,23 +9,26 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *current_node, *next_node; /* traversal ptrs */
+	hash_node_t *current_node = NULL; /* traversal ptrs */
+	hash_node_t *next_node = NULL; /* traversal ptrs */
 	unsigned long int iterator; /* iterator for array */
 
 	if (ht == NULL) /* if hash table is NULL */
 		return; /* return nothing */
 	for (iterator = 0; iterator < ht->size; iterator++) /* traverse array */
 	{
-		current_node = ht->array[iterator]; /* set traversal ptr to head */
+		current_node = ht->array[iterator]; /* set traversal ptr */
 		while (current_node != NULL) /* traverse linked list */
 		{
-			next_node = current_node->next; /* set next node */
 			free(current_node->key); /* free key */
 			free(current_node->value); /* free value */
-			free(current_node); /* free node */
+			next_node = current_node->next; /* set next ptr */
+			free(current_node); /* free current node */
 			current_node = next_node; /* move to next node */
 		}
 	}
 	free(ht->array); /* free array */
-	free(ht); /* free hash tiable */
+	ht->array = NULL; /* set array to NULL */
+	ht->size = 0; /* set size to 0 */
+	free(ht); /* free hash table */
 }
