c179a0c7efe1e5271f0406609fcbe9d47b25b335
internashionalist <taylor.thames@atlasschool.com>
2024-07-11 16:56:27 -0500

    5.0 - base

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table in order of array
+ * @ht: hash table to print
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
