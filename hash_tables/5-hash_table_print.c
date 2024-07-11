e6bd89d1947714437ee397be41039f3e59101eaa
internashionalist <taylor.thames@atlasschool.com>
2024-07-11 17:29:10 -0500

    5.3 - added flag to help print commas correctly, but it seems weird or wrong

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -11,6 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node = NULL; /* traversal ptr for nodes */
 	unsigned long int iterator; /* iterator for array */
+	int first = 1; /* flag for comma */
 
 	if (ht == NULL) /* if hash table is NULL */
 		return; /* return nothing, print nothing */
@@ -19,14 +20,15 @@ void hash_table_print(const hash_table_t *ht)
 
 	for (iterator = 0; iterator < ht->size; iterator++) /* traverse array */
 	{
-	      node = ht->array[iterator]; /* set traversal ptr to head */
-	      while (node != NULL) /* traverse linked list */
-	      {
-		      printf("'%s': '%s'", node->key, node->value); /* print key & value */
-		      if (node->next != NULL) /* if not at tail */
-			      printf(", "); /* print comma */
-		      node = node->next; /* then move to next node */
-	      }
+		node = ht->array[iterator]; /* set traversal ptr to head */
+		while (node != NULL) /* traverse linked list */
+		{
+			if (first == 0) /* if not first node */
+				printf(", "); /* print comma and a space */
+			printf("'%s': '%s'", node->key, node->value); /* print key/value */
+			first = 0; /* set flag to 0 after first node */
+			node = node->next; /* move to next node */
+		}
 	}
-	printf("}\n"); /* print closing brace */
+	printf("}\n"); /* print last brace */
 }
