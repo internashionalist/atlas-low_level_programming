1529436ca70846c89f2d4c5fb5c1e7fd20fe26cc
internashionalist <taylor.thames@atlasschool.com>
2024-07-11 17:48:39 -0500

    6.1 - pointer variables, null case

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,3 +9,9 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *current_node, *next_node; /* traversal ptrs */
+	unsigned long int iterator; /* iterator for array */
+
+	if (ht == NULL) /* if hash table is NULL */
+		return; /* return nothing */
+
