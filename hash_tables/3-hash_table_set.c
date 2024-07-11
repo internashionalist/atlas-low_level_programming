cc39ea1160f8edaae14de402c75632407f289d7c
internashionalist <taylor.thames@atlasschool.com>
2024-07-10 23:28:50 -0500

    3.6 - betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,7 +1,4 @@
 #include "hash_tables.h"
-#include "0-hash_table_create.c"
-#include "1-djb2.c"
-#include "2-key_index.c"
 
 /**
  * hash_table_set - adds an element to hash table
@@ -15,10 +12,10 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL; /* init ptr to hash_node_t struct, set to NULL */
+	hash_node_t *new_node = NULL; /* init ptr to node struct, set to NULL */
 	hash_node_t *current_node = NULL; /* init traversal ptr, set to NULL */
 	unsigned long int index = 0; /* init index for hash table array, set to 0 */
-	char *duplicate = NULL; /* init ptr to duplicated value, set to NULL */
+	char *duplicate = NULL; /* init ptr to duplicated value, NULL set */
 
 	if (!key || !*key || !ht || !value) /* if any parameter is NULL */
 		return (0); /* return 0 - failure */
@@ -27,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (duplicate == NULL) /* if strdup fails */
 		return (0); /* return 0 - failure */
 
-	index = key_index((const unsigned char *)key, ht->size); /* CALL FUNCTION FROM TASK 2 */
+	index = key_index((unsigned char *)key, ht->size); /* get index of key */
 
 	new_node = malloc(sizeof(hash_node_t)); /* malloc for new_node */
 	if (new_node == NULL) /* if malloc fails */
@@ -42,12 +39,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht->array[index] == NULL) /* if index of hash table array is NULL */
 	{
-		ht->array[index] = new_node; /* set index of hash table array to new_node */
+		ht->array[index] = new_node; /* set index of table array to new_node */
 		return (1); /* return 1 - success */
 	}
 
 	current_node = ht->array[index]; /* set current_node to head at index position */
-
 	while (current_node != NULL) /* traverse linked list at index position */
 	{
 		if (strcmp(current_node->key, key) == 0) /* if key is found */
