1d34a7e00948061d7b7c5c02774467a50403c888
internashionalist <taylor.thames@atlasschool.com>
2024-07-12 13:55:14 -0500

    100.2 - won't compile. shit's WRONG.

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -16,3 +16,21 @@ shash_table_t *shash_table_create(unsigned long int size)
 		new_table = malloc(sizeof(shash_table_t));
 		if (new_table == NULL) /* check for malloc failure */
 			return (NULL);
+
+		new_table->size = size; /* size of new_table to size of array */
+		new_table->array = malloc(sizeof(shash_node_t *) * size);
+		if (new_table->array == NULL) /* check for malloc failure */
+		{
+			free(new_table);
+			return (NULL);
+		}
+
+		while (iterator < size) /* traverse array */
+		{
+			new_table->array[iterator] = NULL; /* NULLify ptr in array */
+			iterator++; /* then move on to the next one */
+		}
+	}
+
+	return (new_table); /* return brand spankin' new sorted hash table */
+}
