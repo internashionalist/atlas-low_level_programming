5279049c24d43bccbe799cf5d525b2b6e7ada94c
internashionalist <taylor.thames@atlasschool.com>
2024-07-12 08:20:38 -0500

    100.0 - let's try it.

---
+++ hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/* shash_table_create - creates a sorted hash table
+ * @size: size of array
+ *
+ * Return: pointer to this new SORTED hash table
+ */
+
+shash_table_t *shash_table_create(unsigned long int size)
+{
