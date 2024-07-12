4993a23759420efae4b50ddc18276b3e623e3c7e
internashionalist <taylor.thames@atlasschool.com>
2024-07-12 09:08:31 -0500

    100.1 - variables, malloc

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -8,3 +8,11 @@
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
+	shash_table_t *new_table = NULL;
+	unsigned long int iterator = 0;
+
+	if (size > 0)
+	{
+		new_table = malloc(sizeof(shash_table_t));
+		if (new_table == NULL) /* check for malloc failure */
+			return (NULL);
