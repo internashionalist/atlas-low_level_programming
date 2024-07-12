f3d938152501a065d6cccfa93b2cdb2a028815a3
internashionalist <taylor.thames@atlasschool.com>
2024-07-12 15:35:44 -0500

    100.5 - points don't matter, so I'm using some GPT to check my work as I go, then working backwards to doublecheck and see if I can't learn some stuff I've missed

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -8,4 +8,17 @@
 
 shash_table_t *shash_table_create(unsigned long int size);
 {
-	
+	shash_table_t *new_table = NULL; /* init ptr to hash table, NULL */
+	shash_table_t *ht = malloc(sizeof(shash_table_t));
+	unsigned long int iterator = 0; /* init iterator, 0 */
+
+	if (ht == NULL) /* if malloc fails */
+		return (NULL); /* return NULL - failure */
+
+	ht->size = size; /* set size of new_table to given size */
+	ht->array = malloc(sizeof(shash_node_t *) * size); /* malloc array */
+	if (ht->array == NULL) /* if malloc fails */
+	{
+		free(ht); /* free hash table */
+		return (NULL); /* return NULL - failure */
+	}
