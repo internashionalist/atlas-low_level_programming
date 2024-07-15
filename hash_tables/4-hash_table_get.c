ec609e1ee404adf38d0b9c7f1e1f609f981c1517
internashionalist <taylor.thames@atlasschool.com>
2024-07-15 07:50:36 -0500

    6.10 - readability

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,20 +10,20 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	int index; /* index of key in array */
-	hash_node_t *current_node; /* traversal ptr */
+	int index; /* index of key in hash table */
+	hash_node_t *trav_node; /* traversal ptr for nodes */
 
-	if (ht == NULL || key == NULL) /* if hash table or key is NULL */
+	if (ht == NULL || key == NULL) /* if there is no hash table or key */
 		return (NULL); /* return NULL */
 
 	index = key_index((unsigned char *)key, ht->size); /* FUNCTION - TASK 2 */
 
-	current_node = ht->array[index]; /* set traversal ptr to head of list */
-	while (current_node != NULL) /* traverse linked list at index */
+	trav_node = ht->array[index]; /* set traversal ptr to head of list */
+	while (trav_node != NULL) /* traverse linked list at index */
 	{
-		if (strcmp(current_node->key, key) == 0) /* if key is found */
-			return (current_node->value); /* return value */
-		current_node = current_node->next; /*  or move to next node */
+		if (strcmp(trav_node->key, key) == 0) /* if key is found */
+			return (trav_node->value); /* return value */
+		trav_node = trav_node->next; /*  or move to next node */
 	}
 	return (NULL); /* return NULL if key not found */
 }
--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,7 +9,7 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *current_node = NULL; /* traversal ptrs */
+	hash_node_t *trav_node = NULL; /* traversal ptrs */
 	hash_node_t *next_node = NULL; /* traversal ptrs */
 	unsigned long int iterator; /* iterator for array */
 
@@ -17,14 +17,14 @@ void hash_table_delete(hash_table_t *ht)
 		return; /* return nothing */
 	for (iterator = 0; iterator < ht->size; iterator++) /* traverse array */
 	{
-		current_node = ht->array[iterator]; /* set traversal ptr */
-		while (current_node != NULL) /* traverse linked list */
+		trav_node = ht->array[iterator]; /* set traversal ptr */
+		while (trav_node != NULL) /* traverse linked list */
 		{
-			next_node = current_node->next; /* set next node */
-			free(current_node->key); /* free key */
-			free(current_node->value); /* free value */
-			free(current_node); /* free current node */
-			current_node = next_node; /* set current node to next node */
+			next_node = trav_node->next; /* set next node */
+			free(trav_node->key); /* free key */
+			free(trav_node->value); /* free value */
+			free(trav_node); /* free current node */
+			trav_node = next_node; /* set current node to next node */
 		}
 	}
 	free(ht->array); /* free array */
