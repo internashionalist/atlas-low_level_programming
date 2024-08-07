bddf68e4fbf1ffa5e8aa82e7d540414f9e4a5e3c
internashionalist <taylor.thames@atlasschool.com>
2024-08-07 13:46:07 -0500

    0.1 - compiles, correct

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,8 +7,23 @@
  * @size: number of elements in array
  * @value: value being searched for
  *
- * Return: the first index where value is located, or -1 if value is not
- * present in array or if array is NULL
+ * Return: first index where value is located or -1 if value
+ * not found / NULL array
  */
 
 int linear_search(int *array, size_t size, int value)
+{
+	size_t i; /* index */
+
+	if (array == NULL) /* if array is NULL */
+		return (-1); /* indicate value not found */
+
+	for (i = 0; i < size; i++) /* iterate through array */
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value) /* if value is found */
+			return (i); /* return index where found */
+	}
+
+	return (-1); /* otherwise indicate value not found */
+}
